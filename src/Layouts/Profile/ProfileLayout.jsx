e2310b8b6e990fc074bcd21a108b33944b7baf7e
Obiabo Immanuel <54102389+yhoungdev@users.noreply.github.com>
2022-07-06 15:24:20 +0100

    feat: Layout for profile

---
+++ src/Layouts/Profile/ProfileLayout.jsx
@@ -0,0 +1,30 @@
+const  ProfileLayout = ({children}) => {
+
+
+    return (
+        <>
+        
+            <div>
+
+                {/* profile container  */}
+                <div>
+
+                </div>
+
+                <div className="profile-child">
+
+
+                    {children}
+
+                </div>
+
+
+            </div>
+        
+        </>
+    )
+
+}
+
+
+export  default ProfileLayout;
