bff6718dbb62f8b29b2645b3d19621ea6d5bcf83
Obiabo Immanuel <54102389+yhoungdev@users.noreply.github.com>
2022-07-06 15:19:21 +0100

    feat: added helper functions to make request , and added new color

---
+++ src/Layouts/DashboardLayout.jsx
@@ -0,0 +1,25 @@
+const DashboardLayout = ({ children }) => {
+
+
+    return (
+        <>
+        
+            <div>
+
+                {/* Dashboard layouts goes here  */}
+                <div>
+
+                </div>
+
+                {/* Dashboard child goes here  */}
+                <div>
+                    {children}
+                </div>
+
+            </div>
+        
+        </>
+    )
+
+
+}
