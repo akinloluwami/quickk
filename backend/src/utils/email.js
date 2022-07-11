864c3c281ec80d36f4f777e28fdbece7c53a9900
Akinkunmi <akinkunmiisrael41@gmail.com>
2022-07-11 15:08:53 +0100

    feat: added reset password email template

--- backend/src/utils/email.js
+++ backend/src/utils/email.js
@@ -512,7 +512,7 @@ table, td { color: #000000; } a { color: #0000ee; text-decoration: underline; }
    <td style="overflow-wrap:break-word;word-break:break-word;padding:10px;font-family:arial,helvetica,sans-serif;" align="left">
      
 <div style="color: #ffffff; line-height: 140%; text-align: left; word-wrap: break-word;">
- <p style="font-size: 14px; line-height: 140%;">Need help? ask at <a rel="noopener" href="mailto:support@juicee.fund?subject=&body=" target="_blank">support@juicee.fund</a></p>
+ <p style="font-size: 14px; line-height: 140%;">Ignore this email if you didn't request for a password reset.</p>
 </div>
 
    </td>
@@ -526,7 +526,7 @@ table, td { color: #000000; } a { color: #0000ee; text-decoration: underline; }
    <td style="overflow-wrap:break-word;word-break:break-word;padding:10px;font-family:arial,helvetica,sans-serif;" align="left">
      
 <div style="color: #ffffff; line-height: 140%; text-align: left; word-wrap: break-word;">
- <p style="font-size: 14px; line-height: 140%;">Team Juicee</p>
+ <p style="font-size: 14px; line-height: 140%;">Team Quickk</p>
 </div>
 
    </td>
@@ -534,100 +534,6 @@ table, td { color: #000000; } a { color: #0000ee; text-decoration: underline; }
 </tbody>
 </table>
 
-<!--[if (!mso)&(!IE)]><!--></div><!--<![endif]-->
-</div>
-</div>
-<!--[if (mso)|(IE)]></td><![endif]-->
-   <!--[if (mso)|(IE)]></tr></table></td></tr></table><![endif]-->
- </div>
-</div>
-</div>
-
-
-
-<div class="u-row-container" style="padding: 0px;background-color: transparent">
-<div class="u-row" style="Margin: 0 auto;min-width: 320px;max-width: 400px;overflow-wrap: break-word;word-wrap: break-word;word-break: break-word;background-color: transparent;">
- <div style="border-collapse: collapse;display: table;width: 100%;background-color: transparent;">
-   <!--[if (mso)|(IE)]><table width="100%" cellpadding="0" cellspacing="0" border="0"><tr><td style="padding: 0px;background-color: transparent;" align="center"><table cellpadding="0" cellspacing="0" border="0" style="width:400px;"><tr style="background-color: transparent;"><![endif]-->
-   
-<!--[if (mso)|(IE)]><td align="center" width="400" style="background-color: #843fa1;width: 400px;padding: 0px;border-top: 0px solid transparent;border-left: 0px solid transparent;border-right: 0px solid transparent;border-bottom: 0px solid transparent;border-radius: 0px;-webkit-border-radius: 0px; -moz-border-radius: 0px;" valign="top"><![endif]-->
-<div class="u-col u-col-100" style="max-width: 320px;min-width: 400px;display: table-cell;vertical-align: top;">
-<div style="background-color: #843fa1;width: 100% !important;border-radius: 0px;-webkit-border-radius: 0px; -moz-border-radius: 0px;">
-<!--[if (!mso)&(!IE)]><!--><div style="padding: 0px;border-top: 0px solid transparent;border-left: 0px solid transparent;border-right: 0px solid transparent;border-bottom: 0px solid transparent;border-radius: 0px;-webkit-border-radius: 0px; -moz-border-radius: 0px;"><!--<![endif]-->
-
-<table style="font-family:arial,helvetica,sans-serif;" role="presentation" cellpadding="0" cellspacing="0" width="100%" border="0">
-<tbody>
- <tr>
-   <td style="overflow-wrap:break-word;word-break:break-word;padding:10px;font-family:arial,helvetica,sans-serif;" align="left">
-     
-<div style="color: #ecf0f1; line-height: 140%; text-align: left; word-wrap: break-word;">
- <p style="font-size: 14px; line-height: 140%;"><span style="font-size: 16px; line-height: 22.4px;"><strong>Connect with us on social media<br /></strong></span></p>
-</div>
-
-   </td>
- </tr>
-</tbody>
-</table>
-
-<table style="font-family:arial,helvetica,sans-serif;" role="presentation" cellpadding="0" cellspacing="0" width="100%" border="0">
-<tbody>
- <tr>
-   <td style="overflow-wrap:break-word;word-break:break-word;padding:10px;font-family:arial,helvetica,sans-serif;" align="left">
-     
-<div align="center">
-<div style="display: table; max-width:147px;">
-<!--[if (mso)|(IE)]><table width="147" cellpadding="0" cellspacing="0" border="0"><tr><td style="border-collapse:collapse;" align="center"><table width="100%" cellpadding="0" cellspacing="0" border="0" style="border-collapse:collapse; mso-table-lspace: 0pt;mso-table-rspace: 0pt; width:147px;"><tr><![endif]-->
-
- 
- <!--[if (mso)|(IE)]><td width="32" style="width:32px; padding-right: 5px;" valign="top"><![endif]-->
- <table align="left" border="0" cellspacing="0" cellpadding="0" width="32" height="32" style="width: 32px !important;height: 32px !important;display: inline-block;border-collapse: collapse;table-layout: fixed;border-spacing: 0;mso-table-lspace: 0pt;mso-table-rspace: 0pt;vertical-align: top;margin-right: 5px">
-   <tbody><tr style="vertical-align: top"><td align="left" valign="middle" style="word-break: break-word;border-collapse: collapse !important;vertical-align: top">
-     <a href="https://twitter.com/juiceeHQ" title="Twitter" target="_blank">
-       <img src="https://api.dotjs.dev/image-1.png" alt="Twitter" title="Twitter" width="32" style="outline: none;text-decoration: none;-ms-interpolation-mode: bicubic;clear: both;display: block !important;border: none;height: auto;float: none;max-width: 32px !important">
-     </a>
-   </td></tr>
- </tbody></table>
- <!--[if (mso)|(IE)]></td><![endif]-->
- 
- <!--[if (mso)|(IE)]><td width="32" style="width:32px; padding-right: 5px;" valign="top"><![endif]-->
- <table align="left" border="0" cellspacing="0" cellpadding="0" width="32" height="32" style="width: 32px !important;height: 32px !important;display: inline-block;border-collapse: collapse;table-layout: fixed;border-spacing: 0;mso-table-lspace: 0pt;mso-table-rspace: 0pt;vertical-align: top;margin-right: 5px">
-   <tbody><tr style="vertical-align: top"><td align="left" valign="middle" style="word-break: break-word;border-collapse: collapse !important;vertical-align: top">
-     <a href="https://instagram.com/juiceeHQ" title="Instagram" target="_blank">
-       <img src="https://api.dotjs.dev/image-2.png" alt="Instagram" title="Instagram" width="32" style="outline: none;text-decoration: none;-ms-interpolation-mode: bicubic;clear: both;display: block !important;border: none;height: auto;float: none;max-width: 32px !important">
-     </a>
-   </td></tr>
- </tbody></table>
- <!--[if (mso)|(IE)]></td><![endif]-->
- 
- <!--[if (mso)|(IE)]><td width="32" style="width:32px; padding-right: 5px;" valign="top"><![endif]-->
- <table align="left" border="0" cellspacing="0" cellpadding="0" width="32" height="32" style="width: 32px !important;height: 32px !important;display: inline-block;border-collapse: collapse;table-layout: fixed;border-spacing: 0;mso-table-lspace: 0pt;mso-table-rspace: 0pt;vertical-align: top;margin-right: 5px">
-   <tbody><tr style="vertical-align: top"><td align="left" valign="middle" style="word-break: break-word;border-collapse: collapse !important;vertical-align: top">
-     <a href="https://youtube.com/juiceeHQ" title="YouTube" target="_blank">
-       <img src="https://api.dotjs.dev/image-4.png" alt="YouTube" title="YouTube" width="32" style="outline: none;text-decoration: none;-ms-interpolation-mode: bicubic;clear: both;display: block !important;border: none;height: auto;float: none;max-width: 32px !important">
-     </a>
-   </td></tr>
- </tbody></table>
- <!--[if (mso)|(IE)]></td><![endif]-->
- 
- <!--[if (mso)|(IE)]><td width="32" style="width:32px; padding-right: 0px;" valign="top"><![endif]-->
- <table align="left" border="0" cellspacing="0" cellpadding="0" width="32" height="32" style="width: 32px !important;height: 32px !important;display: inline-block;border-collapse: collapse;table-layout: fixed;border-spacing: 0;mso-table-lspace: 0pt;mso-table-rspace: 0pt;vertical-align: top;margin-right: 0px">
-   <tbody><tr style="vertical-align: top"><td align="left" valign="middle" style="word-break: break-word;border-collapse: collapse !important;vertical-align: top">
-     <a href="https://tiktok.com/juiceeHQ" title="TikTok" target="_blank">
-       <img src="https://api.dotjs.dev/image-3.png" alt="TikTok" title="TikTok" width="32" style="outline: none;text-decoration: none;-ms-interpolation-mode: bicubic;clear: both;display: block !important;border: none;height: auto;float: none;max-width: 32px !important">
-     </a>
-   </td></tr>
- </tbody></table>
- <!--[if (mso)|(IE)]></td><![endif]-->
- 
- 
- <!--[if (mso)|(IE)]></tr></table></td></tr></table><![endif]-->
-</div>
-</div>
-
-   </td>
- </tr>
-</tbody>
-</table>
 
 <!--[if (!mso)&(!IE)]><!--></div><!--<![endif]-->
 </div>
