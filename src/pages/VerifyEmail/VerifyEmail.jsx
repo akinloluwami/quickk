26f1829a0f6d3333be3907c7958703de947708ae
Obiabo Immanuel <54102389+yhoungdev@users.noreply.github.com>
2022-07-11 22:43:46 +0100

    feat: adjusted style for Verifing OTP

--- src/pages/VerifyEmail/VerifyEmail.jsx
+++ src/pages/VerifyEmail/VerifyEmail.jsx
@@ -3,6 +3,8 @@ import { useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
 import OtpInput from "react-otp-input";
 import "./VerifyEmail.scss";
+import AuthLayout from '../../Layouts/AuthLayout';
+import Buttons from "../../components/major/Buttons";
 function VerifyEmail() {
   const [second, setSecond] = useState(0);
   const [count, setCount] = useState(10);
@@ -14,14 +16,12 @@ function VerifyEmail() {
     return () => clearInterval(interval);
   }, [second]);
   return (
-    <div className="ve">
+    <AuthLayout>
+      <div className="ve">
       <div className="logo">
         <Link to="/">
           {" "}
-          <img
-            src="https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png"
-            alt="google logo"
-          />
+     
         </Link>
       </div>
       <form className="form">
@@ -29,6 +29,7 @@ function VerifyEmail() {
 
         <OtpInput
           value={otp}
+          type = "number"
           onChange={(e) => {
             setOtp(e);
           }}
@@ -38,6 +39,7 @@ function VerifyEmail() {
             width: "50px",
             height: "50px",
             borderRadius: "5px",
+            color: "grey",
             fontWeight: "500",
             fontSize: "18px",
             outline: "none",
@@ -46,7 +48,7 @@ function VerifyEmail() {
             margin: "0 5px",
           }}
         />
-        <button>Submit</button>
+        <Buttons value={'Continue'}/>
         <p>
           Didn't receive OTP?{" "}
           <span
@@ -66,6 +68,7 @@ function VerifyEmail() {
         </p>
       </form>
     </div>
+    </AuthLayout>
   );
 }
 
