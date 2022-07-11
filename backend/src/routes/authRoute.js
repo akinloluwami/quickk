18df63b5dc01c82b1f2b311d5b471d886e11f76f
Akinkunmi <akinkunmiisrael41@gmail.com>
2022-07-11 17:29:48 +0100

    feat: added login route

--- backend/src/routes/authRoute.js
+++ backend/src/routes/authRoute.js
@@ -2,6 +2,7 @@ const authController = require("../controllers/authController");
 const router = require("express").Router();
 
 router.post("/signup", authController.signup);
+router.post("/login", authController.login);
 router.post("/verify-email", authController.verifyEmail);
 router.post("/resend-otp", authController.resendOTP);
 router.post("/send-reset-link", authController.sendResetLink);
