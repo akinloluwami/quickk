7c400e58a61b530460cc88648815ad8850096952
Obiabo Immanuel <54102389+yhoungdev@users.noreply.github.com>
2022-07-11 22:32:18 +0100

    feat: made update on Layout

--- src/Layouts/AuthLayout.jsx
+++ src/Layouts/AuthLayout.jsx
@@ -1,27 +1,45 @@
-import { Box , Center, Flex } from "@chakra-ui/react";
+import { Box , Flex } from "@chakra-ui/react";
 import Logo from "../components/minor/Logo";
-import { Link } from "react-router-dom";
-
+import Pattern from './pattern.svg';
+import { Text } from "@chakra-ui/react";
+import ContainerLayout from './ContainerLayout.jsx/ContainerLayout';
 const AuthLayout = ({ children }) => {
 
 
     return (
         <>
 
-            <Flex bg={'var(--bg)'} >
-                <Box bg={'#000'}  width={['50%']} height = {'100vh'} display={['none' , 'none']}>
- 
+            <Flex>
+                <Box bg={'var(--primary-color)'} overflowX='hidden' width={['50%']} height = {'100vh'} display={['none' , 'block']}>
+
                     <Box p='2em'>
                         <Logo/>
                     </Box> 
 
+                    <ContainerLayout>
+                        <Box display={'flex'} h={'50vh'} alignContent={'center'} alignItems='center' >
+
+                            <Box px='1em'>
+
+                                <Text fontWeight={'black'} fontFamily={'var(--primary-font)'} fontSize='5xl' color={'#fff'}>Let Your Imagination Run Wild .</Text>
+                                <Text pr='2em' my={'1em'} color={'#fff'}>Setup your microblog in less than 2 minutes, 
+                                    start wrting and accepting donations in no time.
+                                 </Text>
+                                 
+                            </Box>
+
+                        </Box>
+                    </ContainerLayout>
+
+                    <Box position={'absolute'} bottom='0'>
+                        <img src={Pattern} alt="pattern" />
+                    </Box>
+
                 </Box>
 
-                <Box bg={'var(--bg)'} width={['100%','50%']} h='100vh' margin={'auto'}>
-                        
-                   
+                <Box bg={'var(--bg)'} width={['100%','50%']} h='100vh'>
+                    
                     {children}
-                  
                     
                 </Box>
             </Flex>
