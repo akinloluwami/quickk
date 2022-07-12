311ee8870a9ba350a0aa144efaa9224c50d46683
Obiabo Immanuel <54102389+yhoungdev@users.noreply.github.com>
2022-07-12 22:26:17 +0100

    feat: update request js

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -8,7 +8,7 @@ const DashboardIndex = () => {
 
             <DashboardLayout>
 
-
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto quidem, ipsa quam quod doloribus facere ipsam veritatis veniam nisi iste. Impedit ad iusto dolor veritatis iste soluta placeat temporibus consequatur.
                     
             </DashboardLayout>
            
