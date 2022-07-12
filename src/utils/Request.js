1c00e7eac2e748662ee28596900d57d380eb2288
Akinkunmi <akinkunmiisrael41@gmail.com>
2022-07-12 17:25:36 +0100

    nested properly

--- src/utils/Request.js
+++ src/utils/Request.js
@@ -1,88 +1,65 @@
 import axios from "axios";
-import {useToast} from '@chakra-ui/react';    
-import {ToastContainer , toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { useToast } from "@chakra-ui/react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
- 
 //add the default url
 let defaultUrl = import.meta.env.VITE_APP_BACKEND_URL;
 
-
 //initilize toast
 
+export const postData = async (url, payload, methods) => {
+  try {
+    const response = await axios.post(`${defaultUrl}${url}`, payload, {
+      methods,
+    });
 
-export const postData = async (url, payload, methods ) => {
-    
+    //check if response is success
+    toast.success(response.data.message, {
+      position: "top-center",
+      autoClose: 4000,
+      hideProgressBar: true,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "colored",
+    });
 
-    try {
-
-        const response = await axios.post(`${defaultUrl}${url}`, payload, {methods});
-        
-       
-            //check if response is success
-            toast.success(response.data.message, {
-                position: "top-center",
-                autoClose: 4000,
-                hideProgressBar: true,
-                closeOnClick: true,
-                pauseOnHover: true,
-                draggable: true,
-                progress: undefined,
-                theme: 'colored'
-    
-                });
-                
-
-               
-
-
-            
-            
-                 return { response: response.data, error: false};
-    } catch (error) {
-       
-        const errMsg = error.message;
-        const handleErr = errMsg.response.data
-        toast.error(handleErr, {
-            position: "top-center",
-            autoClose: 5000,
-            hideProgressBar: true,
-            closeOnClick: true,
-            pauseOnHover: true,
-            draggable: true,
-            progress: undefined,
-            theme: 'colored'
-
-            });
-    }
-
-}
+    return { response: response.data, error: false };
+  } catch (error) {
+    const errMsg = error.message;
+    const handleErr = errMsg.response.data;
+    toast.error(handleErr, {
+      position: "top-center",
+      autoClose: 5000,
+      hideProgressBar: true,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "colored",
+    });
+  }
+};
 
 //function to get from the Api
 //get with axios
-export const fetchData = async (url , payloads ) => {
-    
-        try {
-    
-            const response = await axios.get(`${url}`, {payloads});    
-            return response.data;
-    
-        } catch (error) {
-            console.log(error);
-        }
-    
-    }
-    //function to put to the Api
-    //put with axios
+export const fetchData = async (url, payloads) => {
+  try {
+    const response = await axios.get(`${url}`, { payloads });
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+//function to put to the Api
+//put with axios
 export const putData = async (url, data) => {
-        
-            try {
-        
-                const response = await axios.put(`${url}`, data);
-                return response.data;
-        
-            } catch (error) {
-                console.log(error);
-            }
-        
-        }
+  try {
+    const response = await axios.put(`${url}`, data);
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
