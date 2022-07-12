1937022653d028ea751fb38d58219a8195e2d66d
Akinkunmi <akinkunmiisrael41@gmail.com>
2022-07-12 02:54:19 +0100

    minor text changes

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -2,55 +2,71 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 import { Link } from "react-router-dom";
-import {Box ,Center,Flex, Text} from '@chakra-ui/react';
+import { Box, Center, Flex, Text } from "@chakra-ui/react";
 import ContainerLayout from "../../Layouts/ContainerLayout.jsx/ContainerLayout";
-import Inputs from '../../components/major/Inputs';
+import Inputs from "../../components/major/Inputs";
 import AuthLayout from "../../Layouts/AuthLayout";
 import Buttons from "../../components/major/Buttons";
-import { MdOutlineMail , MdOutlinePassword } from 'react-icons/md'
-
+import { MdOutlineMail, MdOutlinePassword } from "react-icons/md";
 
 function Login() {
   return (
     <>
-
       <AuthLayout>
-
-
-
-      <Flex justifyContent={'center'} alignItems='center' py='2em' h='80%'>
-              <Box my='1em' bg={'#fff'} width={['90%','60%']}  py='2em' px={'1em'} > 
-                  
-
-                  <form>
-                    
-                      <Box my='1em'>
-                         <Text fontSize={'xl'} fontWeight={'bold'} fontFamily={'var(--primary-font)'}>  Welcome back  👋 </Text>
-                         <Text my='1em'> Its good to have you back , Enter your details </Text>
-                      </Box>
-
-                     
-                     <Inputs placeholder={'email'} label='Email' type={'email'} icon = { <MdOutlineMail/>} />
-                     <Inputs placeholder={'password'} label='Password' type={'password'} icon = { <MdOutlinePassword/> }/>
-
-                     <Text textAlign={'center'} my='1em'> Forgotten Password  ? <Link to='/forgot-password'> <b> Reset password</b></Link> </Text>
-
-                    <Center>
-                      <Buttons  value={'Login'} bg={'blue.500'} color={'#fff'} width={'100%'}/>
-                    </Center>
-                     
-                     
-                  </form>   
-
+        <Flex justifyContent={"center"} alignItems="center" py="2em" h="80%">
+          <Box my="1em" bg={"#fff"} width={["90%", "60%"]} py="2em" px={"1em"}>
+            <form>
+              <Box my="1em">
+                <Text
+                  fontSize={"xl"}
+                  fontWeight={"bold"}
+                  fontFamily={"var(--primary-font)"}
+                >
+                  {" "}
+                  Welcome back 👋{" "}
+                </Text>
+                <Text my="1em">Login to your Quickk account</Text>
               </Box>
-            </Flex>
 
+              <Inputs
+                placeholder={"Email"}
+                label="Email"
+                type={"email"}
+                icon={<MdOutlineMail />}
+              />
+              <Inputs
+                placeholder={"Password"}
+                label="Password"
+                type={"password"}
+                icon={<MdOutlinePassword />}
+              />
 
+              <Text textAlign={"center"} my="1em">
+                {" "}
+                New to Quickk?{" "}
+                <Link to="/signup">
+                  {" "}
+                  <b>Create an account</b>
+                </Link>{" "}
+              </Text>
+              <Text textAlign={"center"} my="1em">
+                <Link to="/forgot-password">
+                  <p>Forgot password?</p>
+                </Link>{" "}
+              </Text>
+
+              <Center>
+                <Buttons
+                  value={"Login"}
+                  bg={"blue.500"}
+                  color={"#fff"}
+                  width={"100%"}
+                />
+              </Center>
+            </form>
+          </Box>
+        </Flex>
       </AuthLayout>
-
-
-
-
     </>
   );
 }
--- src/pages/Signup/Signup.jsx
+++ src/pages/Signup/Signup.jsx
@@ -2,56 +2,83 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 import { Link } from "react-router-dom";
-import {Box ,Center,Flex, Text} from '@chakra-ui/react';
+import { Box, Center, Flex, Text } from "@chakra-ui/react";
 import ContainerLayout from "../../Layouts/ContainerLayout.jsx/ContainerLayout";
-import Inputs from '../../components/major/Inputs';
+import Inputs from "../../components/major/Inputs";
 import AuthLayout from "../../Layouts/AuthLayout";
 import Buttons from "../../components/major/Buttons";
-import { MdOutlineMail , MdOutlinePassword } from 'react-icons/md'
-import { AiOutlineUser } from 'react-icons/ai'
+import { MdOutlineMail, MdOutlinePassword } from "react-icons/md";
+import { AiOutlineUser } from "react-icons/ai";
 
 function Signup() {
   return (
     <>
-
       <AuthLayout>
-
-
-
-      <Flex justifyContent={'center'} alignItems='center' py='2em' h='80%'>
-              <Box my='1em' bg={'#fff'} width={['90%','60%']}  py='2em' px={'1em'} > 
-                  
-
-                  <form>
-                    
-                      <Box my='1em'>
-                         <Text fontSize={'xl'} fontWeight={'bold'} fontFamily={'var(--primary-font)'}>  Create A Quickk Account 👋 </Text>
-                      </Box>
-
-                     <Inputs placeholder={'display name'} label='Username' type={'text'} icon = {<AiOutlineUser/>} />
-                     <Inputs placeholder={'user name'} label='Username' type={'text'} icon = {<AiOutlineUser/>}/>
-                     <Inputs placeholder={'email'} label='Email' type={'email'} icon ={<MdOutlineMail/>}/>
-                     <Inputs placeholder={'password'} label='Password' type={'password'} icon={ <MdOutlinePassword/> }/>
-                      <Inputs placeholder={'confirm password'} label='Confirm Password' type={'password'} icon={ <MdOutlinePassword/> }/>
-
-
-                      <Text textAlign={'center'} my='1em'> Already had an account ? <Link to='/login'><b>signin</b></Link> </Text>
-
-                      <Center>
-                      <Buttons  value={'Sign up'} bg={'blue.500'} color={'#fff'} width={'100%'}/>
-                    </Center>
-                     
-                  </form>   
-
+        <Flex justifyContent={"center"} alignItems="center" py="2em" h="80%">
+          <Box my="1em" bg={"#fff"} width={["90%", "60%"]} py="2em" px={"1em"}>
+            <form>
+              <Box my="1em">
+                <Text
+                  fontSize={"xl"}
+                  fontWeight={"bold"}
+                  fontFamily={"var(--primary-font)"}
+                >
+                  {" "}
+                  Create A Quickk Account 👋{" "}
+                </Text>
               </Box>
-            </Flex>
 
+              <Inputs
+                placeholder={"Display name"}
+                label="Display name"
+                type={"text"}
+                icon={<AiOutlineUser />}
+              />
+              <Inputs
+                placeholder={"Username"}
+                label="Username"
+                type={"text"}
+                icon={<AiOutlineUser />}
+              />
+              <Inputs
+                placeholder={"Email"}
+                label="Email"
+                type={"Email"}
+                icon={<MdOutlineMail />}
+              />
+              <Inputs
+                placeholder={"Password"}
+                label="Password"
+                type={"Password"}
+                icon={<MdOutlinePassword />}
+              />
+              <Inputs
+                placeholder={"Confirm password"}
+                label="Confirm Password"
+                type={"password"}
+                icon={<MdOutlinePassword />}
+              />
 
+              <Text textAlign={"center"} my="1em">
+                {" "}
+                Already have an account ?{" "}
+                <Link to="/login">
+                  <b>Login</b>
+                </Link>{" "}
+              </Text>
+
+              <Center>
+                <Buttons
+                  value={"Sign up"}
+                  bg={"blue.500"}
+                  color={"#fff"}
+                  width={"100%"}
+                />
+              </Center>
+            </form>
+          </Box>
+        </Flex>
       </AuthLayout>
-
-
-
-
     </>
   );
 }
