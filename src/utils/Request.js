8692e177c97ae617b614f32979f000df5fc0cfcb
Akinkunmi <akinkunmiisrael41@gmail.com>
2022-07-13 08:13:26 +0100

    removed toastify import

--- src/utils/Request.js
+++ src/utils/Request.js
@@ -1,41 +1,34 @@
 import axios from "axios";
-import { useToast } from "@chakra-ui/react";
-import { ToastContainer, toast } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
 
 //add the default url
 let defaultUrl = import.meta.env.VITE_APP_BACKEND_URL;
-
 
 export const postData = async (url, payload) => {
   try {
     const response = await axios.post(`${defaultUrl}${url}`, payload);
     return response;
-    //if the endpoint has data that is returned, return the data as response.data 
-  } catch ( error ) {
-    const err = error.response
+    //if the endpoint has data that is returned, return the data as response.data
+  } catch (error) {
+    const err = error.response;
 
-    //if the endpoint has data that is return, now you will need to 
-    //return error.response.data.message 
+    //if the endpoint has data that is return, now you will need to
+    //return error.response.data.message
     //this will make axios automatically detect the error and display the message
     console.log(err);
     return error;
   }
-}
+};
 
-
-
-//function to fetch data 
+//function to fetch data
 const fetchData = async (url, payload) => {
   try {
     const response = await axios.get(`${defaultUrl}${url}`, payload);
     return response;
-    //this return response is what you will use in your function to get the datas 
-  } catch ( error ) {
-    const err = error.response
-
+    //this return response is what you will use in your function to get the datas
+  } catch (error) {
+    const err = error.response;
 
     console.log(err);
     return error;
   }
-}
+};
