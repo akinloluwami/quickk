1ffae44452b54c4cd5bd399c5e157e32bd684500
Akinkunmi <akinkunmiisrael41@gmail.com>
2022-07-13 08:14:46 +0100

    made the verify email functional

--- src/pages/VerifyEmail/VerifyEmail.jsx
+++ src/pages/VerifyEmail/VerifyEmail.jsx
@@ -5,25 +5,52 @@ import OtpInput from "react-otp-input";
 import "./VerifyEmail.scss";
 import AuthLayout from "../../Layouts/AuthLayout";
 import Buttons from "../../components/major/Buttons";
+import { postData } from "../../utils/Request";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import ReactLoading from "react-loading";
+
 function VerifyEmail() {
   const [second, setSecond] = useState(0);
   const [count, setCount] = useState(30);
   const [otp, setOtp] = useState("");
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
   useEffect(() => {
     const interval = setInterval(() => {
       setSecond(second + 1);
     }, 1000);
     return () => clearInterval(interval);
   }, [second]);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    const data = { otp };
+    const response = await postData(`/auth/verify-email`, data);
+    if (response.status === 200) {
+      toast.success("Verify email sucessful");
+      setLoading(false);
+      setTimeout(() => {
+        navigate("/dashboard");
+      }, 1500);
+    } else {
+      toast.error(response.response.data.message);
+      setLoading(false);
+    }
+  };
+
   return (
     <AuthLayout>
+      <ToastContainer autoClose={2000} />
       <div className="ve">
         <div className="logo">
           <Link to="/"> </Link>
         </div>
         <form className="form">
           <h1>Enter OTP</h1>
-
+          <p>An OTP has been sent to your email</p>
           <OtpInput
             value={otp}
             type="number"
@@ -45,7 +72,24 @@ function VerifyEmail() {
               margin: "0 5px",
             }}
           />
-          <Buttons value={"Continue"} />
+          <Buttons
+            onClick={(e) => {
+              handleSubmit(e);
+            }}
+            disabled={loading}
+            value={
+              loading ? (
+                <ReactLoading
+                  type={"bubbles"}
+                  color={"#fff"}
+                  height={70}
+                  width={70}
+                />
+              ) : (
+                "Complete signup"
+              )
+            }
+          />
           <p>
             Didn't receive OTP?{" "}
             <span
