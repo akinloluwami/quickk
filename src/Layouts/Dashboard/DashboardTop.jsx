1e3f844391a8f7ac4f06eb1b560663710198ee06
Akinkunmi <akinkunmiisrael41@gmail.com>
2022-07-14 16:10:31 +0100

    set default profile picture from initials

--- src/Layouts/Dashboard/DashboardTop.jsx
+++ src/Layouts/Dashboard/DashboardTop.jsx
@@ -8,19 +8,19 @@ import {
   MenuItem,
   MenuList,
 } from "@chakra-ui/react";
-import Logo from './dashboard.svg';
-import {FiLogOut} from 'react-icons/fi'
-import {BiUserCircle} from 'react-icons/bi'
-import {BiChevronDown} from 'react-icons/bi'
+import Logo from "./dashboard.svg";
+import { FiLogOut } from "react-icons/fi";
+import { BiUserCircle } from "react-icons/bi";
+import { BiChevronDown } from "react-icons/bi";
 
-const DashboardTop = ({ displayName }) => {
+const DashboardTop = ({ displayName, profilePic }) => {
   return (
     <>
       <Box
         bg={"#fff"}
         color="#000"
         py={"1em"}
-        px={["1.5em","5em"]}
+        px={["1.5em", "5em"]}
         display={"flex"}
         justifyContent={"space-between"}
       >
@@ -32,22 +32,30 @@ const DashboardTop = ({ displayName }) => {
           <Menu>
             <MenuButton>
               <Flex alignItems={"center"} gap={"1em"}>
-                <Avatar size={"sm"} />
-                <Text fontWeight={"bold"} display={['none' , 'block']}>{displayName}</Text>
-                <Text><BiChevronDown/></Text>
+                <Avatar src={profilePic} />
+                <Text fontWeight={"bold"} display={["none", "block"]}>
+                  {displayName}
+                </Text>
+                <Text>
+                  <BiChevronDown />
+                </Text>
               </Flex>
             </MenuButton>
 
             <MenuList>
               <MenuItem>
-                <Text mr='1em'>
-                    <BiUserCircle/></Text> Profile.
+                <Text mr="1em">
+                  <BiUserCircle />
+                </Text>{" "}
+                Profile.
               </MenuItem>
 
-              <MenuItem> 
-                <Text mr='1em'>
-                    <FiLogOut/></Text>logout
-                </MenuItem>
+              <MenuItem>
+                <Text mr="1em">
+                  <FiLogOut />
+                </Text>
+                logout
+              </MenuItem>
             </MenuList>
           </Menu>
         </Box>
