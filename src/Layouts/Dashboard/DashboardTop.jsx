f30d0ab2ddbc6366529338265553845ac2373468
Akinkunmi <akinkunmiisrael41@gmail.com>
2022-07-14 12:34:41 +0100

    ...

--- src/Layouts/Dashboard/DashboardTop.jsx
+++ src/Layouts/Dashboard/DashboardTop.jsx
@@ -1,17 +1,3 @@
-<<<<<<< HEAD
-import { Avatar, Text, Flex, Box, Menu, MenuButton, MenuItem, MenuList } from '@chakra-ui/react';
-
-import Logo from './dashboard.svg'
-const DashboardTop = () => {
-
-    return (
-        <>
-        
-            <Box bg={'#fff'} color='#000' py={'1em'} px={['1em','5em']} display={'flex'} justifyContent={'space-between'}>
-                   <Box >
-                        <img src={Logo} alt="" />
-                   </Box>
-=======
 import {
   Avatar,
   Text,
@@ -46,28 +32,10 @@ const DashboardTop = ({ displayName }) => {
                 <Text fontWeight={"bold"}>{displayName}</Text>
               </Flex>
             </MenuButton>
->>>>>>> 97c9ed7 (...)
 
             <MenuList>
               <MenuItem>Profile.</MenuItem>
 
-<<<<<<< HEAD
-                            <MenuButton>
-                               <Box display={['none' , 'block']}>
-                                 <Flex alignItems={'center'} gap={'1em'}>
-                                    <Avatar size={'sm'}/>
-                                     <Text fontWeight={'bold'}>Obiabo</Text>
-                                   </Flex>
-                               </Box>
-
-                               <Box display={['block' , 'none']}>
-                                 <Flex alignItems={'center'} gap={'1em'}>
-                                    <Avatar size={'sm'}/>
-                                   
-                                   </Flex>
-                               </Box>
-                            </MenuButton>
-=======
               <MenuItem>logout</MenuItem>
             </MenuList>
           </Menu>
@@ -76,6 +44,5 @@ const DashboardTop = ({ displayName }) => {
     </>
   );
 };
->>>>>>> 97c9ed7 (...)
 
 export default DashboardTop;
