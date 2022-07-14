616abbfd34f3693e25bdc6da1f1712c76db5880b
Obiabo Immanuel <54102389+yhoungdev@users.noreply.github.com>
2022-07-14 11:27:40 +0100

    feat: added icons to sidebar

--- src/Layouts/Dashboard/DashboardTop.jsx
+++ src/Layouts/Dashboard/DashboardTop.jsx
@@ -1,23 +1,33 @@
 import { Avatar, Text, Flex, Box, Menu, MenuButton, MenuItem, MenuList } from '@chakra-ui/react';
-import Logo from '../../components/minor/Logo';
+
+import Logo from './dashboard.svg'
 const DashboardTop = () => {
 
     return (
         <>
         
-            <Box bg={'#fff'} color='#000' py={'1em'} px={'5em'} display={'flex'} justifyContent={'space-between'}>
-                   <Box>
-                        <Logo/>
+            <Box bg={'#fff'} color='#000' py={'1em'} px={['1em','5em']} display={'flex'} justifyContent={'space-between'}>
+                   <Box >
+                        <img src={Logo} alt="" />
                    </Box>
 
                    <Box>
                         <Menu>
 
                             <MenuButton>
-                                <Flex alignItems={'center'} gap={'1em'}>
+                               <Box display={['none' , 'block']}>
+                                 <Flex alignItems={'center'} gap={'1em'}>
                                     <Avatar size={'sm'}/>
-                                    <Text fontWeight={'bold'}>Obiabo</Text>
-                                </Flex>
+                                     <Text fontWeight={'bold'}>Obiabo</Text>
+                                   </Flex>
+                               </Box>
+
+                               <Box display={['block' , 'none']}>
+                                 <Flex alignItems={'center'} gap={'1em'}>
+                                    <Avatar size={'sm'}/>
+                                   
+                                   </Flex>
+                               </Box>
                             </MenuButton>
 
                             <MenuList>
