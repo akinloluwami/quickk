81279aaf51a509683bd7b780e351dc0e833efd0f
Obiabo Immanuel <54102389+yhoungdev@users.noreply.github.com>
2022-07-14 12:31:30 +0100

    feat: icons

--- src/Layouts/Dashboard/DashboardTop.jsx
+++ src/Layouts/Dashboard/DashboardTop.jsx
@@ -1,48 +1,56 @@
-import {
-  Avatar,
-  Text,
-  Flex,
-  Box,
-  Menu,
-  MenuButton,
-  MenuItem,
-  MenuList,
-} from "@chakra-ui/react";
-import Logo from "../../components/minor/Logo";
-const DashboardTop = ({ displayName }) => {
-  return (
-    <>
-      <Box
-        bg={"#fff"}
-        color="#000"
-        py={"1em"}
-        px={"5em"}
-        display={"flex"}
-        justifyContent={"space-between"}
-      >
-        <Box>
-          <Logo />
-        </Box>
+import { Avatar, Text, Flex, Box, Menu, MenuButton, MenuItem, MenuList } from '@chakra-ui/react';
+import {BiChevronDown} from 'react-icons/bi';
+import {FiLogOut} from 'react-icons/fi';
+import {BiUserCircle} from 'react-icons/bi';
+import Logo from './dashboard.svg'
+const DashboardTop = () => {
 
-        <Box>
-          <Menu>
-            <MenuButton>
-              <Flex alignItems={"center"} gap={"1em"}>
-                <Avatar size={"sm"} />
-                <Text fontWeight={"bold"}>{displayName}</Text>
-              </Flex>
-            </MenuButton>
+    return (
+        <>
+        
+            <Box bg={'#fff'} color='#000' py={'1em'} px={['1em','5em']} display={'flex'} justifyContent={'space-between'}>
+                   <Box >
+                        <img src={Logo} alt="" />
+                   </Box>
 
-            <MenuList>
-              <MenuItem>Profile.</MenuItem>
+                   <Box>
+                        <Menu>
 
-              <MenuItem>logout</MenuItem>
-            </MenuList>
-          </Menu>
-        </Box>
-      </Box>
-    </>
-  );
-};
+                            <MenuButton>
+                               <Box display={['none' , 'block']}>
+                                 <Flex alignItems={'center'} gap={'1em'}>
+                                    <Avatar size={'sm'}/>
+                                     <Text>Obiabo</Text>
+                                     <BiChevronDown/>
+                                   </Flex>
+                               </Box>
 
-export default DashboardTop;
+                               <Box display={['block' , 'none']}>
+                                 <Flex alignItems={'center'} gap={'1em'}>
+                                    <Avatar size={'sm'}/>
+                                    <BiChevronDown/>
+                                   
+                                   </Flex>
+                               </Box>
+                            </MenuButton>
+
+                            <MenuList>
+                               
+                                <MenuItem>
+                                   <Text mr='2'><BiUserCircle/></Text> Profile.
+                                </MenuItem>
+
+                                <MenuItem>
+                                  <Text mr='0.5em'><FiLogOut/></Text>  logout
+                                </MenuItem>
+
+                            </MenuList>
+                        </Menu>
+                   </Box>
+            </Box>
+          
+        </>
+    )
+}
+
+export default DashboardTop
