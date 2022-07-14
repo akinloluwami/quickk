3daf61f57db39566990e129eabbb7a8b8bb23e94
Obiabo Immanuel <54102389+yhoungdev@users.noreply.github.com>
2022-07-14 14:20:56 +0100

    feat: Added mobile tabs for dashboard

--- src/Layouts/Dashboard/DashboardLayout.jsx
+++ src/Layouts/Dashboard/DashboardLayout.jsx
@@ -1,4 +1,5 @@
 import { Box, Text, Flex } from "@chakra-ui/react";
+import MobileNav from "./Dashboard Components/MobileNav";
 import DashboardTop from "./DashboardTop";
 import Sidebar from "./Sidebar";
 
@@ -18,9 +19,29 @@ const DashboardLayout = ({ children }) => {
           {/* Main display sections  */}
           <Box p="2em">
             <Box>{children}</Box>
+            
           </Box>
+
         </Flex>
+        
+
+
+        {/* Display mobile tab on small device  */}
+
+         <Box display={['block' , 'none']}
+          position={'fixed'}
+          right={'0'}
+          left={'0'}
+          bottom={'0'}
+          >
+          
+            <MobileNav/>
+         </Box>
+
+        {/* mobile view tab end  */}
       </Box>
+
+      
     </>
   );
 };
