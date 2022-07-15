c6e9b67a30b89ecb19de31f788c0a7d9d4ab7205
Obiabo Immanuel <54102389+yhoungdev@users.noreply.github.com>
2022-07-15 22:01:09 +0100

    feat: post componetn

---
+++ src/components/Post/PostBox.jsx
@@ -0,0 +1,15 @@
+import { Box } from '@chakra-ui/react';
+const PostBox = ({ blogTitle , blogBrief  }) => {
+
+    return (
+        <>
+        
+            <Box>
+
+            </Box>
+        
+        </>
+    )
+}
+
+export default PostBox;
