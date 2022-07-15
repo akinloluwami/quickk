2b4ee4665903b68cf30452c203b9ae18ea7ffe48
Akinkunmi <akinkunmiisrael41@gmail.com>
2022-07-16 00:51:23 +0100

    fix: fixed unfollow feature

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -57,6 +57,63 @@ module.exports = {
     });
   },
 
-  unfollowUser: async (req, res) => {},
+  unfollowUser: async (req, res) => {
+    const token = req.headers.authorization.split(" ")[1];
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const userFollowing = decoded.uuid;
+    const userToUnfollow = req.query.username;
+    console.log(userFollowing, userToUnfollow);
+
+    if (!userFollowing || !userToUnfollow) {
+      return res.status(400).json({
+        message: "Username is required",
+      });
+    }
+    const user = await User.findOne({
+      where: {
+        uuid: userFollowing,
+      },
+    });
+    const userToUnfollowObj = await User.findOne({
+      where: {
+        username: userToUnfollow,
+      },
+    });
+    if (!userToUnfollowObj) {
+      return res.status(400).json({
+        message: "User does not exist",
+      });
+    }
+    if (!user) {
+      return res.status(400).json({
+        message: "User not found",
+      });
+    }
+    if (!userToUnfollowObj.followers.includes(userFollowing)) {
+      return res.status(400).json({
+        message: "User not followed",
+      });
+    }
+    if (userFollowing === userToUnfollow) {
+      return res.status(400).json({
+        message: "You cannot unfollow yourself",
+      });
+    }
+    user.update({
+      following: user.following.filter(
+        (following) => following !== userToUnfollowObj.uuid
+      ),
+    });
+    userToUnfollowObj.update({
+      followers: userToUnfollowObj.followers.filter(
+        (follower) => follower !== userFollowing
+      ),
+    });
+    await user.save();
+    await userToUnfollowObj.save();
+    return res.status(200).json({
+      message: `You are no longer following ${userToUnfollow}`,
+    });
+  },
 };
 //   /*****************************************************/
