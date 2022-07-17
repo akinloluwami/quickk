b736889c260c1418f351b94590a79b7ace06a14a
Akinkunmi <akinkunmiisrael41@gmail.com>
2022-07-17 06:42:27 +0100

    removed get posts from user route

--- backend/src/routes/postRoute.js
+++ backend/src/routes/postRoute.js
@@ -2,7 +2,6 @@ const postController = require("../controllers/postController");
 const router = require("express").Router();
 
 router.post("/create", postController.createPost);
-router.get("/user/posts", postController.getPostsFromUser);
 router.get("/user/post/:slug", postController.getSinglePostFromUser);
 
 module.exports = router;
