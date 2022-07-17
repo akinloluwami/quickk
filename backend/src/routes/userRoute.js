da566c95de3d9dd4a4b7db910ddff08127b50790
Akinkunmi <akinkunmiisrael41@gmail.com>
2022-07-17 06:40:36 +0100

    feat: add get user profile and posts route

--- backend/src/routes/userRoute.js
+++ backend/src/routes/userRoute.js
@@ -3,5 +3,7 @@ const router = require("express").Router();
 
 router.post("/follow", userController.followUser);
 router.post("/unfollow", userController.unfollowUser);
+router.post("/posts", userController.getUserPosts);
+router.post("/profile", userController.getUserProfile);
 
 module.exports = router;
