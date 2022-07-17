644dfc9fea7be4f57345f46886bcc87de7348c88
Akinkunmi <akinkunmiisrael41@gmail.com>
2022-07-17 23:54:28 +0100

    feat: added like, unlike and comment routes

--- backend/src/routes/postRoute.js
+++ backend/src/routes/postRoute.js
@@ -3,5 +3,8 @@ const router = require("express").Router();
 
 router.post("/create", postController.createPost);
 router.get("/user/post/:slug", postController.getSinglePostFromUser);
+router.post("/like", postController.likePost);
+router.post("/unlike", postController.unlikePost);
+router.post("/comment", postController.commentOnPost);
 
 module.exports = router;
