11e7b7df63010a7473d1a597b68e7d45d4da0f5f
Akinkunmi <akinkunmiisrael41@gmail.com>
2022-07-18 17:40:17 +0100

    feat: added get followers route

--- backend/src/routes/userRoute.js
+++ backend/src/routes/userRoute.js
@@ -6,8 +6,5 @@ router.post("/unfollow", userController.unfollowUser);
 router.get("/posts", userController.getUserPosts);
 router.get("/profile", userController.getUserProfile);
 router.get("/followers", userController.getUserFollowers);
-router.get("/following", userController.getUserFollowing);
-router.post("/add-wallet-info", userController.addWalletInfo);
-router.get("/get-wallet-info", userController.getWalletInfo);
 
 module.exports = router;
