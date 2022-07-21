d59d79078af06f379fe76f5f555a4c6028f914e3
Akinkunmi <akinkunmiisrael41@gmail.com>
2022-07-21 10:01:45 +0100

    added cover image button and title input

--- src/pages/Dashboard/Write.jsx
+++ src/pages/Dashboard/Write.jsx
@@ -1,9 +1,16 @@
 import DashboardLayout from "../../Layouts/Dashboard/DashboardLayout";
-import { Box } from "@chakra-ui/react";
+import { Box, Input, Button, Flex } from "@chakra-ui/react";
 const Write = () => {
   return (
     <>
-      <DashboardLayout>{/*Text editor goes here */}</DashboardLayout>
+      <DashboardLayout>
+        <Flex justifyContent={"center"} flexDirection={"column"}>
+          <Flex margin={"10px"}>
+            <Button variantColor={"teal"}>Add Cover Image</Button>
+          </Flex>
+          <Input placeholder={"Title"} />
+        </Flex>
+      </DashboardLayout>
     </>
   );
 };
