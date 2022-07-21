00a76dedeabae5a5a0d8e80d2ca22fdd9f609c9e
Akinkunmi <akinkunmiisrael41@gmail.com>
2022-07-21 11:41:39 +0100

    disabled publish button if the title is less than 10 characters or image is uploading

--- src/pages/Dashboard/Write.jsx
+++ src/pages/Dashboard/Write.jsx
@@ -6,6 +6,7 @@ const Write = () => {
   const inputRef = useRef();
   const [image, setImage] = useState(null);
   const [uploading, setUploading] = useState(false);
+  const [postTitle, setPostTitle] = useState("");
 
   const handleClick = () => {
     inputRef.current.click();
@@ -53,42 +54,50 @@ const Write = () => {
               backgroundColor={"#0031af"}
               color={"#fff"}
               _hover={{ backgroundColor: "#0031af" }}
+              disabled={
+                !postTitle || !image || uploading || postTitle.length < 10
+              }
             >
               Publish
             </Button>
           </Flex>
-          <Box
-            display={"flex"}
-            justifyContent={"center"}
-            alignItems={"center"}
-            margin={"10px"}
-            borderRadius={"10px"}
-            backgroundColor={"#fff"}
-            padding={"10px"}
-            width={"100%"}
-            height={"400px"}
-            overflow={"hidden"}
-            position={"relative"}
-          >
-            <Button
-              onClick={() => {}}
-              position={"absolute"}
-              top={"15px"}
-              left={"15px"}
+          {/* {image && (
+            <Box
+              display={"flex"}
+              justifyContent={"center"}
+              alignItems={"center"}
+              margin={"10px"}
+              borderRadius={"10px"}
               backgroundColor={"#fff"}
-              color={"#0031af"}
-              title={"Remove Cover Image"}
+              padding={"10px"}
+              width={"100%"}
+              height={"400px"}
+              overflow={"hidden"}
+              position={"relative"}
             >
-              <FaTimes fontSize={"1.5em"} color={"red"} />
-            </Button>
-            <img src={image} alt="" />
-          </Box>
+              <Button
+                onClick={() => {}}
+                position={"absolute"}
+                top={"15px"}
+                left={"15px"}
+                backgroundColor={"#fff"}
+                color={"#0031af"}
+                title={"Remove Cover Image"}
+              >
+                <FaTimes fontSize={"1.5em"} color={"red"} />
+              </Button>
+              <img src={image} alt="" />
+            </Box>
+          )} */}
           <Input
             placeholder={"Article Title..."}
             fontSize={"2em"}
             height={"1em"}
             fontWeight={"500"}
             py={"1em"}
+            onChange={(e) => {
+              setPostTitle(e.target.value);
+            }}
           />
         </Flex>
       </DashboardLayout>
