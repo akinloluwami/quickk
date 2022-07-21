30efbe3716e408428125583df6c5300503f0b2ed
Akinkunmi <akinkunmiisrael41@gmail.com>
2022-07-21 10:44:28 +0100

    added file change handler

--- src/pages/Dashboard/Write.jsx
+++ src/pages/Dashboard/Write.jsx
@@ -1,12 +1,37 @@
 import DashboardLayout from "../../Layouts/Dashboard/DashboardLayout";
 import { Box, Input, Button, Flex } from "@chakra-ui/react";
+import { useRef, useState } from "react";
 const Write = () => {
+  const inputRef = useRef();
+  const [displayImage, setDisplayImage] = useState("");
+  const handleClick = () => {
+    inputRef.current.click();
+  };
+  const handleFileChange = (event) => {
+    const fileObj = event.target.files && event.target.files[0];
+    if (!fileObj) {
+      return;
+    }
+    console.log("fileObj is", fileObj);
+    event.target.value = null;
+    console.log(event.target.files);
+    console.log(fileObj);
+    console.log(fileObj.name);
+  };
   return (
     <>
       <DashboardLayout>
         <Flex justifyContent={"center"} flexDirection={"column"}>
           <Flex margin={"10px"} justifyContent={"space-between"}>
-            <Button variantColor={"teal"}>Add Cover Image</Button>
+            <Input
+              ref={inputRef}
+              type={"file"}
+              display={"none"}
+              onChange={handleFileChange}
+            />
+            <Button variantcolor={"teal"} onClick={handleClick}>
+              Add Cover Image
+            </Button>
             <Button
               backgroundColor={"#0031af"}
               color={"#fff"}
