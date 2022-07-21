4ca167cff0cf6a79e7977b52ecd54b0f0ed5a3a1
Akinkunmi <akinkunmiisrael41@gmail.com>
2022-07-21 11:42:55 +0100

    removed console.log

--- src/Layouts/Dashboard/DashboardLayout.jsx
+++ src/Layouts/Dashboard/DashboardLayout.jsx
@@ -23,7 +23,6 @@ const DashboardLayout = ({ children }) => {
       setDisplayName(res.data.displayName);
       setFollowing(res.data.following);
       setFollowers(res.data.followers);
-      console.log(res.data);
     });
     setProfilePic(
       `https://avatars.dicebear.com/api/initials/${displayName}.svg`
