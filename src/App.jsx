37618d7c4d526a9f4128345438930cf7a4ac2d1f
Obiabo Immanuel <54102389+yhoungdev@users.noreply.github.com>
2022-07-22 02:43:49 +0100

    feat: restructure code and pass userName

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,32 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import ForgotPassword from "./pages/ForgotPassword/ForgotPassword";
-import Home from "./pages/Home/Home";
-import Login from "./pages/Login/Login";
-import ResetPassword from "./pages/ResetPassword/ResetPassword";
-import Signup from "./pages/Signup/Signup";
-import VerifyEmail from "./pages/VerifyEmail/VerifyEmail";
 import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-
 import { useEffect } from "react";
-import DashboardIndex from "./pages/Dashboard/Dashboard";
-import Draft from "./pages/Dashboard/Draaft";
-import Donations from "./pages/Dashboard/Donations";
-import Write from "./pages/Dashboard/Write";
-import Posts from './pages/Dashboard/Posts';
-import Scheduled from './pages/Dashboard/Scheduled';
-import Newslatter from './pages/Dashboard/Newslatter';
-import Analytics from './pages/Dashboard/Analytics';
-import Profile from './pages/Profile/profile';
-import PostIndex from "./pages/Post/PostIndex";
-import ProfileDonations from "./pages/Profile/profileDonations";
-import NotFound from "./pages/NotFound/NotFound";
-import Protected from "./Routes/ProtectedRoute";
 import ProtectedRoutes from "./Routes/ProtectedRoute";
+import { UserNameProvider} from './context/userName';
 
 function App() {
   return (
     <>
+      <UserNameProvider>
       <ToastContainer />
 
-      <Router>
-        
-          {/* <Route path="/" element={<Home />} />
-         
-   */}
+        <Router>
 
 
-          {/* Dynamic routes  */}
-          {/* <Route path='/post/:id' element={<PostIndex/>} />
-          <Route path='/profile-donations' element={<ProfileDonations/>} /> */}
 
 
-          {/* Not found  */}
-          {/* <Route path="*" element={<NotFound/>} /> */}
+  
+        {/* <Route path='/post/:id' element={<PostIndex/>} />
+        <Route path='/profile-donations' element={<ProfileDonations/>} /> */}
 
-           
-          <ProtectedRoutes/>
 
-       
-      </Router>
+
+        <ProtectedRoutes/>
+
+
+          </Router>
+      </UserNameProvider>
     </>
   );
 }
--- src/pages/Dashboard/Posts.jsx
+++ src/pages/Dashboard/Posts.jsx
@@ -2,6 +2,7 @@ import DashboardLayout from "../../Layouts/Dashboard/DashboardLayout";
 import { Box, Input, Button, Flex, Text, Textarea } from "@chakra-ui/react";
 import { FaTimes } from "react-icons/fa";
 import { fetchData } from "../../utils/Request";
+import { UserNameContext } from '../../context/userName';
 
 const Posts = () => {
  
@@ -12,6 +13,7 @@ const Posts = () => {
           <Text>Posts</Text>
         </Box>
       </DashboardLayout>
+      
     </>
   );
 };
