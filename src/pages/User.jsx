5ab36280ff5b228bee57279a154037a845ab4306
Akinkunmi <akinkunmiisrael41@gmail.com>
2022-07-22 19:28:50 +0100

    added user page

---
+++ src/pages/User.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+function User() {
+  return (
+    <div>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint laudantium
+      aliquid qui hic sequi cum doloremque ea aut, aperiam eos dicta nisi
+      provident, praesentium nihil voluptates in inventore impedit quos labore.
+      Voluptatem officiis commodi at cupiditate facere alias asperiores, autem
+      sit molestiae provident, dicta tenetur eaque dolorem, perspiciatis
+      aspernatur? Amet incidunt eligendi, blanditiis minima, id molestias soluta
+      eveniet ut delectus suscipit, voluptate qui recusandae fuga at officia
+      deleniti labore eaque corporis aspernatur quos. Nihil sit placeat
+      perspiciatis in eos adipisci odit iste ab! Suscipit, molestias cum.
+      Aperiam molestias vel, consectetur officia labore at cumque vitae, quas
+      eos iste, adipisci magnam.
+    </div>
+  );
+}
+
+export default User;
