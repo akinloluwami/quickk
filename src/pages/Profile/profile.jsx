be2ef1b64729b30a0ae7fef2c06418c772974721
Akinkunmi <akinkunmiisrael41@gmail.com>
2022-07-23 00:11:32 +0100

    fixed indentation

--- src/pages/Profile/profile.jsx
+++ src/pages/Profile/profile.jsx
@@ -1,25 +1,21 @@
-import {Box, Center} from "@chakra-ui/react";
+import { Box, Center } from "@chakra-ui/react";
 import DashboardLayout from "../../Layouts/Dashboard/DashboardLayout";
 import ProfileLayout from "../../Layouts/Profile/ProfileLayout";
-import BlogBox from './Components/BlogBox';
+import BlogBox from "./Components/BlogBox";
 
 const Profile = () => {
-    return (
-        <>
-
-           <DashboardLayout>
-
-                    <ProfileLayout>
-                        <Box>
-                            <BlogBox/>
-                            <BlogBox/>
-                        </Box>
-                    </ProfileLayout>
-
-           </DashboardLayout>
-        
-        </>
-    )
-}
+  return (
+    <>
+      <DashboardLayout>
+        <ProfileLayout>
+          <Box>
+            <BlogBox />
+            <BlogBox />
+          </Box>
+        </ProfileLayout>
+      </DashboardLayout>
+    </>
+  );
+};
 
 export default Profile;
