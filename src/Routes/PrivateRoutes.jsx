769610a2b020e90ba8a9fa21c2e432ff9e6a6aac
Akinkunmi <akinkunmiisrael41@gmail.com>
2022-07-22 19:12:18 +0100

    added the required pages

--- src/Routes/PrivateRoutes.jsx
+++ src/Routes/PrivateRoutes.jsx
@@ -1,46 +1,35 @@
 import Home from "../pages/Home/Home";
 import Protected from "./ProtectedRoute";
-import { Route , Routes , BrowserRouter as Router } from "react-router-dom";
+import { Route, Routes, BrowserRouter as Router } from "react-router-dom";
 import DashboardIndex from "../pages/Dashboard/Dashboard";
 import Donations from "../pages/Dashboard/Donations";
 import Draft from "../pages/Dashboard/Draaft";
 import Write from "../pages/Dashboard/Write";
-
 import Scheduled from "../pages/Dashboard/Scheduled";
 import Newslatter from "../pages/Dashboard/Newslatter";
 import Analytics from "../pages/Dashboard/Analytics";
+import Posts from "../pages/Dashboard/Posts";
 import Profile from "../pages/Profile/profile";
-import NotFound from '../pages/NotFound/NotFound';
-
+import NotFound from "../pages/NotFound/NotFound";
 
 const Private = () => {
-    return (
-        <>
-
-
-
-
-                <Routes>
-                    
-                    <Route path="*" element={<NotFound/>} />
-                    <Route path="/dashboard" element={<DashboardIndex />} />
-                    <Route path="/dashboard/draft" element={<Draft />} />
-                    <Route path="/dashboard/donations" element={<Donations />} />
-                    <Route path="/dashboard/explore" element={<Donations />} />
-                  
-                  
-                    <Route path="/dashboard/scheduled" element={<Scheduled />} />
-                    <Route path="/dashboard/newsletter" element={<Newslatter />} />
-                    <Route path="/dashboard/analytics" element={<Analytics />} />
-                    <Route path="/profile" element={<Profile />} /> 
-
-                </Routes>
-            
-            
-          
-           
-        </>
-    )
-}
+  return (
+    <>
+      <Routes>
+        <Route path="*" element={<NotFound />} />
+        <Route path="/dashboard" element={<DashboardIndex />} />
+        <Route path="/dashboard/draft" element={<Draft />} />
+        <Route path="/dashboard/write" element={<Write />} />
+        <Route path="/dashboard/donations" element={<Donations />} />
+        <Route path="/dashboard/explore" element={<Donations />} />
+        <Route path="/dashboard/posts" element={<Posts />} />
+        <Route path="/dashboard/scheduled" element={<Scheduled />} />
+        <Route path="/dashboard/newsletter" element={<Newslatter />} />
+        <Route path="/dashboard/analytics" element={<Analytics />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </>
+  );
+};
 
 export default Private;
