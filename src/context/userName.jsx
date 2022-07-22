36c0c0129f7a7f28bb9cf5909b17651b18c9a795
Akinkunmi <akinkunmiisrael41@gmail.com>
2022-07-22 19:11:06 +0100

    fixed indentation

--- src/context/userName.jsx
+++ src/context/userName.jsx
@@ -1,48 +1,38 @@
-//createApi text to userName 
-import { createContext , useState  , useEffect} from 'react';
-import { fetchData } from '../utils/Request';
+//createApi text to userName
+import { createContext, useState, useEffect } from "react";
+import { fetchData } from "../utils/Request";
 
 const UserNameContext = createContext();
 
-
 //function for context
-const UserNameProvider  = ({ children }) => {
+const UserNameProvider = ({ children }) => {
+  const [userName, setUserName] = useState();
 
-    const [ userName , setUserName ] = useState()
+  useEffect(() => {
+    try {
+      const response = fetchData("/dashboard/user/profile", {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+      }).then((response) => {
+        //get userName
+        const userName = response?.data?.username;
+        //set the username to state
+        setUserName(userName);
+        console.clear();
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  });
 
-    useEffect( () => {
-       
-        try {
+  return (
+    //now set pass userName publicly and call it anywhere in your app
+    <UserNameContext.Provider value={userName}>
+      {children}
+    </UserNameContext.Provider>
+  );
+};
 
-            const response = fetchData('/dashboard/user/profile' , {
-                headers: {
-                    "Content-Type": "application/json",
-                    Authorization: `Bearer ${localStorage.getItem("token")}`,
-                  },
-            }).then ( response => {
-                //get userName
-                const userName = response?.data?.username;
-                //set the username to state 
-                setUserName(userName)
-                console.clear()
-                
-            })
-
-        } catch ( error ) {
-            console.log(error)
-        }
-
-        
-    })
-
-    return (
-        
-        //now set pass userName publicly and call it anywhere in your app
-        <UserNameContext.Provider  value={userName}>
-            {children}
-        </UserNameContext.Provider>
-
-    )
-}
-
-export  {UserNameContext , UserNameProvider};
+export { UserNameContext, UserNameProvider };
