370a133b24a7a59927af08774d9b529f8ac6cd01
Akinkunmi <akinkunmiisrael41@gmail.com>
2022-07-23 23:50:34 +0100

    removed dashboard layout and added conditional rendering

--- src/pages/Profile/profile.jsx
+++ src/pages/Profile/profile.jsx
@@ -2,18 +2,49 @@ import { Box, Center } from "@chakra-ui/react";
 import DashboardLayout from "../../Layouts/Dashboard/DashboardLayout";
 import ProfileLayout from "../../Layouts/Profile/ProfileLayout";
 import BlogBox from "./Components/BlogBox";
+import { fetchData } from "../../utils/Request";
+import { useState, useEffect } from "react";
 
 const Profile = () => {
+  const username = window.location.pathname.split("/")[1];
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  useEffect(() => {
+    const response = fetchData(`/user/profile/${username}`);
+    response.then((data) => {
+      if (data.status === 200) {
+        setLoading(false);
+      } else {
+        setError(true);
+        setErrorMessage("User not found");
+        setLoading(false);
+      }
+    });
+  }, []);
   return (
     <>
-      <DashboardLayout>
+      {loading ? (
+        <Center>
+          <Box>
+            <Box>Loading...</Box>
+          </Box>
+        </Center>
+      ) : error ? (
+        <Center>
+          <Box>
+            <Box>{errorMessage}</Box>
+          </Box>
+        </Center>
+      ) : (
         <ProfileLayout>
           <Box>
             <BlogBox />
             <BlogBox />
           </Box>
         </ProfileLayout>
-      </DashboardLayout>
+      )}
     </>
   );
 };
