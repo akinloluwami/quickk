86df18aee39f45336dde8d726cbbe6e367b6c0b8
Akinkunmi <akinkunmiisrael41@gmail.com>
2022-07-23 04:12:56 +0100

    added delete request

--- src/utils/Request.js
+++ src/utils/Request.js
@@ -35,4 +35,15 @@ const fetchData = async (url, payload) => {
   }
 };
 
-export { postData, fetchData };
+const deleteData = async (url, payload) => {
+  try {
+    const response = await axios.delete(`${defaultUrl}${url}`, payload);
+    return response;
+    //this return response is what you will use in your function to get the datas
+  } catch (error) {
+    const err = error.response;
+    return error;
+  }
+};
+
+export { postData, fetchData, deleteData };
