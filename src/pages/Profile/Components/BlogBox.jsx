fa9e75a2e50542773c5b80b55a5e2187bba1c63f
Akinkunmi <akinkunmiisrael41@gmail.com>
2022-07-24 02:24:23 +0100

    updated...

--- src/pages/Profile/Components/BlogBox.jsx
+++ src/pages/Profile/Components/BlogBox.jsx
@@ -1,13 +1,14 @@
-import { Box, Flex, Img, Text } from "@chakra-ui/react";
+import { Box, Flex, Img, Text, Link } from "@chakra-ui/react";
 import { Avatar } from "@chakra-ui/react";
 import { AiOutlineComment, AiOutlineMore, AiFillHeart } from "react-icons/ai";
 import { BiDonateBlood } from "react-icons/bi";
 import { BsCalendarDate } from "react-icons/bs";
-
-const BlogBox = ({ blogTitle, blogBrief }) => {
+import moment from "moment";
+const BlogBox = ({ title, likes, views, content, date }) => {
   return (
     <>
       <Box
+        boxShadow="lg"
         bg={"#fff"}
         width={["100%", "100%"]}
         my={"1em"}
@@ -18,15 +19,19 @@ const BlogBox = ({ blogTitle, blogBrief }) => {
           <Flex flexDir={["column", "row"]} alignItems={"center"}>
             <Box my={"1em"}>
               <Text fontWeight={"bold"} fontSize={["1.2em", "1.5em"]}>
-                This is the title for your post goes here{" "}
+                {title}
               </Text>
               <Text my={"1em"}>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
-                doloribus nam sapiente debitis laborum omnis,{" "}
+                {content.length > 100
+                  ? content.substring(0, 100) + "..."
+                  : content}
               </Text>
 
               <Box display={"flex"} gap={"1em"}>
-                <BsCalendarDate /> <Text> 20th July, 2022</Text>
+                <BsCalendarDate /> <Text>{moment(date).fromNow()}</Text>
+              </Box>
+              <Box display={"flex"} gap={"1em"}>
+                <AiFillHeart /> <Text>{likes}</Text>
               </Box>
             </Box>
           </Flex>
