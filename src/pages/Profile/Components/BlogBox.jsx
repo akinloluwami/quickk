5325d74cd6ef8f5e6036f34ae76dc4ccabd91813
Akinkunmi <akinkunmiisrael41@gmail.com>
2022-07-25 00:48:36 +0100

    added cover image and other things

--- src/pages/Profile/Components/BlogBox.jsx
+++ src/pages/Profile/Components/BlogBox.jsx
@@ -4,19 +4,30 @@ import { AiOutlineComment, AiOutlineMore, AiFillHeart } from "react-icons/ai";
 import { BiDonateBlood } from "react-icons/bi";
 import { BsCalendarDate } from "react-icons/bs";
 import moment from "moment";
-const BlogBox = ({ title, likes, views, content, date }) => {
+const BlogBox = ({ title, likes, views, content, date, coverImage }) => {
   return (
     <>
       <Box
         boxShadow="lg"
         bg={"#fff"}
-        width={["100%", "100%"]}
         my={"1em"}
-        p={["1em", "2em"]}
+        p="1em"
         borderRadius={"1em"}
+        width="300px"
+        mx={"3em"}
       >
+        {coverImage && (
+          <Img
+            src={coverImage}
+            alt=""
+            width={["100%", "400px"]}
+            height="200px"
+            objectFit={"cover"}
+            borderRadius={"10px"}
+          />
+        )}
         <Box>
-          <Flex flexDir={["column", "row"]} alignItems={"center"}>
+          <Flex flexDir={["column"]} alignItems={"center"}>
             <Box my={"1em"}>
               <Text fontWeight={"bold"} fontSize={["1.2em", "1.5em"]}>
                 {title}
