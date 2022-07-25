7a6e5690bd401e540a6396466c1703d89dd68d53
Akinkunmi <akinkunmiisrael41@gmail.com>
2022-07-25 15:45:45 +0100

    added page title

--- src/pages/Profile/Donate.jsx
+++ src/pages/Profile/Donate.jsx
@@ -1,10 +1,34 @@
 import { Box, Text } from "@chakra-ui/react";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ProfileLayout from "../../Layouts/Profile/ProfileLayout";
+import { Helmet } from "react-helmet";
+import { fetchData, postData } from "../../utils/Request";
 
 function Donate() {
+  const username = window.location.pathname.split("/")[1];
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+  const [displayName, setDisplayName] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  useEffect(() => {
+    const response = fetchData(`/user/profile/${username}`);
+    response.then((data) => {
+      if (data.status === 200) {
+        setDisplayName(data.data.user.displayName);
+        setLoading(false);
+      } else {
+        setError(true);
+        setErrorMessage("User not found");
+        setLoading(false);
+      }
+    });
+  }, []);
+
   return (
     <>
+      <Helmet>
+        <title>{displayName} | Donate</title>
+      </Helmet>
       <ProfileLayout>
         <Box>
           <Text>Donate</Text>
