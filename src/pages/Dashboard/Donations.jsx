0dc0bd59705c036bf97a81f9d28c54c9fa6d43fc
Akinkunmi <akinkunmiisrael41@gmail.com>
2022-07-25 15:44:14 +0100

    added page title and fixed indentation

--- src/pages/Dashboard/Donations.jsx
+++ src/pages/Dashboard/Donations.jsx
@@ -1,21 +1,20 @@
 import { Center, Box } from "@chakra-ui/react";
 import DashboardLayout from "../../Layouts/Dashboard/DashboardLayout";
+import { Helmet } from "react-helmet";
 
 const Donations = () => {
-    return (
-        <>
-        
-            <DashboardLayout>
-            <Box>
-
-                <center>
-                    No donations yet !
-                </center>
-                </Box>
-            </DashboardLayout>
-
-        </>
-    )
-}
+  return (
+    <>
+      <Helmet>
+        <title>Donations | Quickk Dashboard</title>
+      </Helmet>
+      <DashboardLayout>
+        <Box>
+          <center>No donations yet !</center>
+        </Box>
+      </DashboardLayout>
+    </>
+  );
+};
 
 export default Donations;
