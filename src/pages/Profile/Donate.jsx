22cf5711a54cbd715e0e568b1c915e108daba414
Obiabo Immanuel <54102389+yhoungdev@users.noreply.github.com>
2022-07-25 23:09:28 +0100

    feat: fixed conflict

--- src/pages/Profile/Donate.jsx
+++ src/pages/Profile/Donate.jsx
@@ -1,5 +1,5 @@
 import { Box, Text } from "@chakra-ui/react";
-import React from "react";
+import React, {useState , useEffect} from "react";
 import ContainerLayout from "../../Layouts/ContainerLayout.jsx/ContainerLayout";
 import ProfileLayout from "../../Layouts/Profile/ProfileLayout";
 import { Helmet } from "react-helmet";
