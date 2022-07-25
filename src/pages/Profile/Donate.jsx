db9647aa2566f7e1fad7c3c79e6ae379f8fb798d
Akinkunmi <akinkunmiisrael41@gmail.com>
2022-07-25 01:04:56 +0100

    setup donate page

---
+++ src/pages/Profile/Donate.jsx
@@ -0,0 +1,17 @@
+import { Box, Text } from "@chakra-ui/react";
+import React from "react";
+import ProfileLayout from "../../Layouts/Profile/ProfileLayout";
+
+function Donate() {
+  return (
+    <>
+      <ProfileLayout>
+        <Box>
+          <Text>Donate</Text>
+        </Box>
+      </ProfileLayout>
+    </>
+  );
+}
+
+export default Donate;
