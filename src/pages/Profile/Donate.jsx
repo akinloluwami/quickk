da68b3b68e904ba9138a6353f74e664199ca03d4
Akinkunmi <akinkunmiisrael41@gmail.com>
2022-07-26 10:38:23 +0100

    added donate functionality

--- src/pages/Profile/Donate.jsx
+++ src/pages/Profile/Donate.jsx
@@ -1,9 +1,11 @@
-import { Box, Text } from "@chakra-ui/react";
-import React, {useState , useEffect} from "react";
+import { Box, Button, Input, Text, Textarea } from "@chakra-ui/react";
+import React, { useState, useEffect } from "react";
 import ContainerLayout from "../../Layouts/ContainerLayout.jsx/ContainerLayout";
 import ProfileLayout from "../../Layouts/Profile/ProfileLayout";
 import { Helmet } from "react-helmet";
 import { fetchData, postData } from "../../utils/Request";
+import { useLazerpay } from "lazerpay-react";
+import { usePayercoins } from "payercoins-react";
 
 function Donate() {
   const username = window.location.pathname.split("/")[1];
@@ -11,6 +13,10 @@ function Donate() {
   const [error, setError] = useState(false);
   const [displayName, setDisplayName] = useState("");
   const [errorMessage, setErrorMessage] = useState("");
+  const [amount, setAmount] = useState(0);
+  const [message, setMessage] = useState("");
+  const [success, setSuccess] = useState(false);
+  const [uniqueIdentifier, setUniqueIdentifier] = useState("");
   useEffect(() => {
     const response = fetchData(`/user/profile/${username}`);
     response.then((data) => {
@@ -25,17 +31,78 @@ function Donate() {
     });
   }, []);
 
+  const uniqueIdentifierGenerator = () => {
+    const randomString = Math.random().toString(36).substring(2, 12);
+    setUniqueIdentifier(randomString);
+  };
+
+  useEffect(() => {
+    uniqueIdentifierGenerator();
+  }, []);
+  const config = {
+    publicKey: import.meta.env.VITE_APP_LAZER_PAY_PUBLIC_KEY,
+    currency: "USD",
+    amount: amount,
+    reference:
+      username + "-" + Date.now() + "-" + displayName + uniqueIdentifier,
+    acceptPartialPayment: false,
+    onSuccess: (response) => {
+      console.log(response);
+      setSuccess(true);
+    },
+    onClose: () => {
+      console.log("closed");
+    },
+    onError: (response) => {
+      console.log(response);
+    },
+  };
+
+  const initializePayment = useLazerpay(config);
+
   return (
     <>
       <Helmet>
         <title>{displayName} | Donate</title>
       </Helmet>
       <ContainerLayout>
-      <ProfileLayout>
-        <Box>
-          <Text>Donate</Text>
-        </Box>
-      </ProfileLayout>
+        <ProfileLayout>
+          <Box width={"400px"}>
+            <Text
+              fontSize="22px"
+              fontWeight="bold"
+              color="black"
+              textAlign="center"
+              marginBottom="1rem"
+            >
+              How much do you want to donate?
+            </Text>
+            <Input
+              type="number"
+              placeholder="Enter amount in USD"
+              marginBottom={"1rem"}
+              onChange={(e) => setAmount(e.target.value)}
+            />
+            <Textarea
+              placeholder={`Enter a message for ${displayName} (optional)`}
+              marginBottom={"1rem"}
+            />
+            <Button
+              width={"100%"}
+              color={"white"}
+              borderRadius={"20px"}
+              bg={"#0031af"}
+              _hover={{ bg: "#19315f" }}
+              marginBottom={"1rem"}
+              onClick={() => {
+                initializePayment();
+              }}
+              disabled={amount === 0}
+            >
+              Donate
+            </Button>
+          </Box>
+        </ProfileLayout>
       </ContainerLayout>
     </>
   );
