86a00e96641d70dc57d5ce3ddc4e2e8cda06e649
Akinkunmi <akinkunmiisrael41@gmail.com>
2022-07-26 22:34:53 +0100

    set up basic boilerplate for the functionality

--- src/pages/Dashboard/Links.jsx
+++ src/pages/Dashboard/Links.jsx
@@ -1,9 +1,25 @@
 import { useState, useEffect } from "react";
 import DashboardLayout from "../../Layouts/Dashboard/DashboardLayout";
 import { Helmet } from "react-helmet";
-import { Text, Box } from "@chakra-ui/react";
+import { Text, Box, Button, Input, Flex } from "@chakra-ui/react";
+import { fetchData, postData, deleteData } from "../../utils/Request";
 
 const Links = () => {
+  const [linkInputs, setlinkInputs] = useState([]);
+  const [linkTitle, setLinkTitle] = useState("");
+  const [linkAddress, setLinkAddress] = useState("");
+  const [updating, setUpdating] = useState(false);
+
+  const handleAddLink = () => {
+    const newLinkInputs = [...linkInputs];
+    const input = {
+      title: linkTitle,
+      link: linkAddress,
+      deleting: false,
+    };
+    newLinkInputs.push(input);
+    setlinkInputs(newLinkInputs);
+  };
   return (
     <>
       <Helmet>
@@ -11,7 +27,42 @@ const Links = () => {
       </Helmet>
       <DashboardLayout>
         <Box>
-          <Text>Add Links</Text>
+          <Button onClick={handleAddLink}>Add New Link</Button>
+          <Box>
+            {linkInputs.map((link) => (
+              <Box
+                key={linkInputs.indexOf(link)}
+                my={4}
+                bg={"#fff"}
+                boxShadow="md"
+                height={"150px"}
+                width={"100%"}
+              >
+                <Input
+                  value={link.title}
+                  onChange={(e) => setLinkTitle(e.target.value)}
+                />
+                <Input
+                  value={link.link}
+                  onChange={(e) => setLinkAddress(e.target.value)}
+                />
+                <Button
+                  onClick={() => {
+                    const newLinkInputs = [...linkInputs];
+                    newLinkInputs[linkInputs.indexOf(link)].deleting = true;
+                    setlinkInputs(newLinkInputs);
+                    setTimeout(() => {
+                      const newLinkInputs = [...linkInputs];
+                      newLinkInputs.splice(linkInputs.indexOf(link), 1);
+                      setlinkInputs(newLinkInputs);
+                    }, 1000);
+                  }}
+                >
+                  {link.deleting ? "Deleting..." : "Delete"}
+                </Button>
+              </Box>
+            ))}
+          </Box>
         </Box>
       </DashboardLayout>
     </>
