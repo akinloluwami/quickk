30262072ed496fc25ee40fcf22d80701e9678eea
Akinkunmi <akinkunmiisrael41@gmail.com>
2022-07-26 23:28:35 +0100

    ..

--- backend/src/routes/dashboardRoute.js
+++ backend/src/routes/dashboardRoute.js
@@ -8,9 +8,8 @@ router.get("/page-views", dashboardController.getPageViews);
 router.post("/user/update", dashboardController.updateProfile);
 router.post("/user/update/dp", dashboardController.updateProfilePicture);
 router.delete("/user/delete/dp", dashboardController.deleteProfilePicture);
-router.post("/links/add", dashboardController.addLink);
-router.delete("/links/delete/:id", dashboardController.deleteLink);
 router.get("/links/get", dashboardController.getLinks);
+router.post("/links/add", dashboardController.addLink);
 
 module.exports = router;
 
