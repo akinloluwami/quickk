9eacff183c54ec7695313cb427bf21cfe326a38c
Akinkunmi <akinkunmiisrael41@gmail.com>
2022-07-27 01:10:27 +0100

    added update link route

--- backend/src/routes/dashboardRoute.js
+++ backend/src/routes/dashboardRoute.js
@@ -11,6 +11,7 @@ router.delete("/user/delete/dp", dashboardController.deleteProfilePicture);
 router.get("/links/get", dashboardController.getLinks);
 router.post("/links/add", dashboardController.addLink);
 router.delete("/links/delete/:id", dashboardController.deleteLink);
+router.post("/links/update/:id", dashboardController.updateLink);
 
 module.exports = router;
 
