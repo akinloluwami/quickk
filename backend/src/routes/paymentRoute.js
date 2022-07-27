b18c214ba8173684fa5e45c6bc3e8ef038327d15
Akinkunmi <akinkunmiisrael41@gmail.com>
2022-07-27 21:51:46 +0100

    added donation route

--- backend/src/routes/paymentRoute.js
+++ backend/src/routes/paymentRoute.js
@@ -1,7 +1,7 @@
 const payments = require("../controllers/paymentsController");
 const router = require("express").Router();
 
-router.post("/donate", payments.donateToUser);
+router.post("/donate", payments.donate);
 router.post("/update", payments.updateWalletAddressAndMinimumDonationAmount);
 router.get("/get", payments.getWalletAddressAndMinimumDonationAmount);
 
