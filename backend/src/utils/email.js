4bb7d91c8603bda2cadaa58ed06e11fbd3271359
Akinkunmi <akinkunmiisrael41@gmail.com>
2022-07-28 14:10:23 +0100

    added donation email handler

--- backend/src/utils/email.js
+++ backend/src/utils/email.js
@@ -566,8 +566,265 @@ table, td { color: #000000; } a { color: #0000ee; text-decoration: underline; }
     }
   });
 }
+function donationEmail(email, subject, from, message) {
+  const transporter = nodeMailer.createTransport({
+    host: "server53.web-hosting.com",
+    port: 465,
+    secure: true,
+    auth: {
+      user: process.env.SMTP_EMAIL,
+      pass: process.env.SMTP_PASSWORD,
+    },
+  });
+  const mailOptions = {
+    from: from,
+    to: email,
+    subject: subject,
+    html: `
+    <!DOCTYPE HTML PUBLIC "-//W3C//DTD XHTML 1.0 Transitional //EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xmlns:v="urn:schemas-microsoft-com:vml" xmlns:o="urn:schemas-microsoft-com:office:office">
+<head>
+<!--[if gte mso 9]>
+<xml>
+<o:OfficeDocumentSettings>
+ <o:AllowPNG/>
+ <o:PixelsPerInch>96</o:PixelsPerInch>
+</o:OfficeDocumentSettings>
+</xml>
+<![endif]-->
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="x-apple-disable-message-reformatting">
+<!--[if !mso]><!--><meta http-equiv="X-UA-Compatible" content="IE=edge"><!--<![endif]-->
+<title></title>
+
+ <style type="text/css">
+   @media only screen and (min-width: 420px) {
+.u-row {
+ width: 400px !important;
+}
+.u-row .u-col {
+ vertical-align: top;
+}
+
+.u-row .u-col-100 {
+ width: 400px !important;
+}
+
+}
+
+@media (max-width: 420px) {
+.u-row-container {
+ max-width: 100% !important;
+ padding-left: 0px !important;
+ padding-right: 0px !important;
+}
+.u-row .u-col {
+ min-width: 320px !important;
+ max-width: 100% !important;
+ display: block !important;
+}
+.u-row {
+ width: calc(100% - 40px) !important;
+}
+.u-col {
+ width: 100% !important;
+}
+.u-col > div {
+ margin: 0 auto;
+}
+}
+body {
+margin: 0;
+padding: 0;
+}
+
+table,
+tr,
+td {
+vertical-align: top;
+border-collapse: collapse;
+}
+
+p {
+margin: 0;
+}
+
+.ie-container table,
+.mso-container table {
+table-layout: fixed;
+}
+
+* {
+line-height: inherit;
+}
+
+a[x-apple-data-detectors='true'] {
+color: inherit !important;
+text-decoration: none !important;
+}
+
+table, td { color: #000000; } a { color: #0000ee; text-decoration: underline; }
+ </style>
+
+
+
+</head>
+
+<body class="clean-body u_body" style="margin: 0;padding: 0;-webkit-text-size-adjust: 100%;background-color: #ebebeb;color: #000000">
+<!--[if IE]><div class="ie-container"><![endif]-->
+<!--[if mso]><div class="mso-container"><![endif]-->
+<table style="border-collapse: collapse;table-layout: fixed;border-spacing: 0;mso-table-lspace: 0pt;mso-table-rspace: 0pt;vertical-align: top;min-width: 320px;Margin: 0 auto;background-color: #ebebeb;width:100%" cellpadding="0" cellspacing="0">
+<tbody>
+<tr style="vertical-align: top">
+ <td style="word-break: break-word;border-collapse: collapse !important;vertical-align: top">
+ <!--[if (mso)|(IE)]><table width="100%" cellpadding="0" cellspacing="0" border="0"><tr><td align="center" style="background-color: #ebebeb;"><![endif]-->
+ 
+
+<div class="u-row-container" style="padding: 0px;background-color: transparent">
+<div class="u-row" style="Margin: 0 auto;min-width: 320px;max-width: 400px;overflow-wrap: break-word;word-wrap: break-word;word-break: break-word;background-color: transparent;">
+ <div style="border-collapse: collapse;display: table;width: 100%;background-color: transparent;">
+   <!--[if (mso)|(IE)]><table width="100%" cellpadding="0" cellspacing="0" border="0"><tr><td style="padding: 0px;background-color: transparent;" align="center"><table cellpadding="0" cellspacing="0" border="0" style="width:400px;"><tr style="background-color: transparent;"><![endif]-->
+   
+<!--[if (mso)|(IE)]><td align="center" width="400" style="background-color: #292828;width: 400px;padding: 0px;border-top: 0px solid transparent;border-left: 0px solid transparent;border-right: 0px solid transparent;border-bottom: 0px solid transparent;border-radius: 0px;-webkit-border-radius: 0px; -moz-border-radius: 0px;" valign="top"><![endif]-->
+<div class="u-col u-col-100" style="max-width: 320px;min-width: 400px;display: table-cell;vertical-align: top;">
+<div style="background-color: #292828;width: 100% !important;border-radius: 0px;-webkit-border-radius: 0px; -moz-border-radius: 0px;">
+<!--[if (!mso)&(!IE)]><!--><div style="padding: 0px;border-top: 0px solid transparent;border-left: 0px solid transparent;border-right: 0px solid transparent;border-bottom: 0px solid transparent;border-radius: 0px;-webkit-border-radius: 0px; -moz-border-radius: 0px;"><!--<![endif]-->
+
+<table style="font-family:arial,helvetica,sans-serif;" role="presentation" cellpadding="0" cellspacing="0" width="100%" border="0">
+<tbody>
+ <tr>
+   <td style="overflow-wrap:break-word;word-break:break-word;padding:10px;font-family:arial,helvetica,sans-serif;" align="left">
+     
+<table width="100%" cellpadding="0" cellspacing="0" border="0">
+<tr>
+ <td style="padding-right: 0px;padding-left: 0px;" align="center">
+   
+   <img align="center" border="0" src="https://api.dotjs.dev/quickk.png" alt="" title="" style="outline: none;text-decoration: none;-ms-interpolation-mode: bicubic;clear: both;display: inline-block !important;border: none;height: auto;float: none;width: 35%;max-width: 133px;" width="133"/>
+   
+ </td>
+</tr>
+</table>
+
+   </td>
+ </tr>
+</tbody>
+</table>
+
+<table style="font-family:arial,helvetica,sans-serif;" role="presentation" cellpadding="0" cellspacing="0" width="100%" border="0">
+<tbody>
+ <tr>
+   <td style="overflow-wrap:break-word;word-break:break-word;padding:10px;font-family:arial,helvetica,sans-serif;" align="left">
+     
+<div style="color: #ffffff; line-height: 140%; text-align: left; word-wrap: break-word;">
+</div>
+
+   </td>
+ </tr>
+</tbody>
+</table>
+
+<table style="font-family:arial,helvetica,sans-serif;" role="presentation" cellpadding="0" cellspacing="0" width="100%" border="0">
+<tbody>
+ <tr>
+   <td style="overflow-wrap:break-word;word-break:break-word;padding:10px;font-family:arial,helvetica,sans-serif;" align="left">
+     
+<div style="color: #ecf0f1; line-height: 140%; text-align: left; word-wrap: break-word;">
+ <p style="font-size: 14px; line-height: 140%;">${message}</p>
+</div>
+
+   </td>
+ </tr>
+</tbody>
+</table>
+
+<table style="font-family:arial,helvetica,sans-serif;" role="presentation" cellpadding="0" cellspacing="0" width="100%" border="0">
+<tbody>
+ <tr>
+   <td style="overflow-wrap:break-word;word-break:break-word;padding:10px;font-family:arial,helvetica,sans-serif;" align="left">
+
+   </td>
+ </tr>
+</tbody>
+</table>
+
+<table style="font-family:arial,helvetica,sans-serif;" role="presentation" cellpadding="0" cellspacing="0" width="100%" border="0">
+<tbody>
+ <tr>
+   <td style="overflow-wrap:break-word;word-break:break-word;padding:10px;font-family:arial,helvetica,sans-serif;" align="left">
+     
+<div align="center">
+<!--[if mso]><table width="100%" cellpadding="0" cellspacing="0" border="0" style="border-spacing: 0; border-collapse: collapse; mso-table-lspace:0pt; mso-table-rspace:0pt;font-family:arial,helvetica,sans-serif;"><tr><td style="font-family:arial,helvetica,sans-serif;" align="center"><v:roundrect xmlns:v="urn:schemas-microsoft-com:vml" xmlns:w="urn:schemas-microsoft-com:office:word" href="" style="height:42px; v-text-anchor:middle; width:102px;" arcsize="9.5%" stroke="f" fillcolor="#843fa1"><w:anchorlock/><center style="color:#FFFFFF;font-family:arial,helvetica,sans-serif;"><![endif]-->
+ <a href="" target="_blank" style="box-sizing: border-box;display: inline-block;font-family:arial,helvetica,sans-serif;text-decoration: none;-webkit-text-size-adjust: none;text-align: center;color: #FFFFFF; background-color: #843fa1; border-radius: 4px;-webkit-border-radius: 4px; -moz-border-radius: 4px; width:auto; max-width:100%; overflow-wrap: break-word; word-break: break-word; word-wrap:break-word; mso-border-alt: none;">
+   
+ </a>
+<!--[if mso]></center></v:roundrect></td></tr></table><![endif]-->
+</div>
+
+   </td>
+ </tr>
+</tbody>
+</table>
+
+<table style="font-family:arial,helvetica,sans-serif;" role="presentation" cellpadding="0" cellspacing="0" width="100%" border="0">
+<tbody>
+ <tr>
+   <td style="overflow-wrap:break-word;word-break:break-word;padding:10px;font-family:arial,helvetica,sans-serif;" align="left">
+
+   </td>
+ </tr>
+</tbody>
+</table>
+
+<table style="font-family:arial,helvetica,sans-serif;" role="presentation" cellpadding="0" cellspacing="0" width="100%" border="0">
+<tbody>
+ <tr>
+   <td style="overflow-wrap:break-word;word-break:break-word;padding:10px;font-family:arial,helvetica,sans-serif;" align="left">
+     
+<div style="color: #ffffff; line-height: 140%; text-align: left; word-wrap: break-word;">
+ <p style="font-size: 14px; line-height: 140%;">Team Quickk</p>
+</div>
+
+   </td>
+ </tr>
+</tbody>
+</table>
+
+
+<!--[if (!mso)&(!IE)]><!--></div><!--<![endif]-->
+</div>
+</div>
+<!--[if (mso)|(IE)]></td><![endif]-->
+   <!--[if (mso)|(IE)]></tr></table></td></tr></table><![endif]-->
+ </div>
+</div>
+</div>
+
+
+ <!--[if (mso)|(IE)]></td></tr></table><![endif]-->
+ </td>
+</tr>
+</tbody>
+</table>
+<!--[if mso]></div><![endif]-->
+<!--[if IE]></div><![endif]-->
+</body>
+
+</html>
+
+    `,
+  };
+  transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log(`📨 Email has been sent to ${email} 💌`);
+    }
+  });
+}
 
 module.exports = {
   otpEmail,
   resetPasswordEmail,
+  donationEmail,
 };
