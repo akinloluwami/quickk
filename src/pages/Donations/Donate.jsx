d96ab41487404bcca40659ed7fb798d2332665c1
Akinkunmi <akinkunmiisrael41@gmail.com>
2022-07-28 14:30:19 +0100

    fecth donations

--- src/pages/Donations/Donate.jsx
+++ src/pages/Donations/Donate.jsx
@@ -1,20 +1,42 @@
-import { Box , Text} from '@chakra-ui/react';
-import DonationBox from '../Profile/Components/DonationBox';
+import { Box, Text } from "@chakra-ui/react";
+import DonationBox from "../Profile/Components/DonationBox";
+import { useState, useEffect } from "react";
+import { fetchData, postData } from "../../utils/Request";
 
 const Donate = () => {
+  const [donations, setDonations] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
-    return (
-        <>
-        
-            
-                <Box maxW={['100%','70%']} mx={'auto'} py={'2em'}>
-                    <Text></Text>
-                    <DonationBox/>
-                </Box>
-
-        </>
-    )
-}
-
+  useEffect(() => {
+    const response = fetchData("/payment/get-donations", {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
+      },
+    });
+    response
+      .then((data) => {
+        setDonations(data.data.donations);
+        console.log(data.data.donations);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+  return (
+    <>
+      <Box maxW={["100%", "70%"]} mx={"auto"} py={"2em"}>
+        <Text></Text>
+        {isLoading ? (
+          <Text>Loading...</Text>
+        ) : donations.length > 0 ? (
+          donations.map((donation) => <DonationBox key={donation.id} />)
+        ) : (
+          <Text>No donations yet</Text>
+        )}
+      </Box>
+    </>
+  );
+};
 
 export default Donate;
