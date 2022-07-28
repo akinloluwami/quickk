3cb799251deb4934af252373714d127a60afcb91
Akinkunmi <akinkunmiisrael41@gmail.com>
2022-07-28 12:46:57 +0100

    added edit post route

--- backend/src/routes/postRoute.js
+++ backend/src/routes/postRoute.js
@@ -2,6 +2,7 @@ const postController = require("../controllers/postController");
 const router = require("express").Router();
 
 router.post("/create", postController.createPost);
+router.post("/edit", postController.editPost);
 router.get("/user/post/:slug", postController.getSinglePostFromUser);
 router.post("/like", postController.likePost);
 router.post("/unlike", postController.unlikePost);
