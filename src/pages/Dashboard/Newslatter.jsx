f6c855eac6961411398f56f20e243d29efb7872d
Akinkunmi <akinkunmiisrael41@gmail.com>
2022-07-30 00:30:57 +0100

    added page title

--- src/pages/Dashboard/Newslatter.jsx
+++ src/pages/Dashboard/Newslatter.jsx
@@ -2,8 +2,12 @@ import { Center, Box } from "@chakra-ui/react";
 import DashboardLayout from "../../Layouts/Dashboard/DashboardLayout";
 // import { Helmet } from "react-helmet";
 import CommingSoon from "../../components/major/CommingSoon";
+import { useEffect } from "react";
 
 const Newslatter = () => {
+  useEffect(() => {
+    document.title = "Newsletter | Quickk Dashboard";
+  }, []);
   return (
     <>
       {/* <Helmet>
