8e318ee58c7f276b72c204f9182ee84c6cace733
Akinkunmi <akinkunmiisrael41@gmail.com>
2022-07-30 00:30:24 +0100

    added page title

--- src/pages/Dashboard/Analytics.jsx
+++ src/pages/Dashboard/Analytics.jsx
@@ -2,8 +2,13 @@ import { Center, Box } from "@chakra-ui/react";
 import DashboardLayout from "../../Layouts/Dashboard/DashboardLayout";
 // import { Helmet } from "react-helmet";
 import CommingSoon from "../../components/major/CommingSoon";
+import { useEffect } from "react";
 
 const Analytics = () => {
+  useEffect(() => {
+    document.title = "Analytics | Quickk Dashboard";
+  }, []);
+
   return (
     <>
       {/* <Helmet>
