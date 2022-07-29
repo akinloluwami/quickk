dffbf7eb2f68413953f99eef0a5f62b16361d44b
Obiabo Immanuel <54102389+yhoungdev@users.noreply.github.com>
2022-07-29 15:14:38 +0100

    feat: adding header

--- src/components/HomeKits/TopBar.jsx
+++ src/components/HomeKits/TopBar.jsx
@@ -1,8 +1,22 @@
+import { Box } from '@chakra-ui/react';
 const TopBar = () => {
+
   return (
     <>
 
-      
+      <Box>
+        {/* Logo goes here  */}
+        <Box>
+
+
+        </Box>
+
+
+        <Box>
+          {/* Navbar goes here  */}
+          
+        </Box>
+      </Box>
     
     </>
   )
