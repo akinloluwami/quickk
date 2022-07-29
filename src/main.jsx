92ad4f749d61745766704d7374e5bbc671e860e7
Akinkunmi <akinkunmiisrael41@gmail.com>
2022-07-29 16:39:47 +0100

    mtchewww

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
-import { ChakraProvider } from '@chakra-ui/react'
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import { ChakraProvider } from "@chakra-ui/react";
+import React from "react";
+import * as ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
