0de1759b110ac7128eb729dfa183c8edb6e4b73c
Akinkunmi <akinkunmiisrael41@gmail.com>
2022-07-30 10:38:36 +0100

    fixed?

--- src/pages/Dashboard/Edit.jsx
+++ src/pages/Dashboard/Edit.jsx
@@ -229,8 +229,22 @@ const EditPost = () => {
               setPostTitle(e.target.value);
             }}
           />
-
-          <Editor
+          <Textarea
+            placeholder={"Write your article..."}
+            fontSize={"1.5em"}
+            height={"100%"}
+            fontWeight={"500"}
+            py={"1em"}
+            px={"1em"}
+            borderRadius={"10px"}
+            border={"1px solid #0031af"}
+            marginTop={"10px"}
+            onChange={(e) => {
+              setPostContent(e.target.value);
+            }}
+            value={postContent}
+          />
+          {/* <Editor
             text={postContent}
             options={{
               toolbar: {
@@ -262,7 +276,7 @@ const EditPost = () => {
               fontWeight: "400",
               overflowY: "scroll",
             }}
-          />
+          /> */}
         </Flex>
       </DashboardLayout>
     </>
