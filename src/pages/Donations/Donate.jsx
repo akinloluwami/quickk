c33c3646d568c3772debc59a16b66b7cf3fed2ea
Akinkunmi <akinkunmiisrael41@gmail.com>
2022-07-31 12:51:26 +0100

    imported request donation modal

--- src/pages/Donations/Donate.jsx
+++ src/pages/Donations/Donate.jsx
@@ -1,13 +1,15 @@
-import { Box, Flex, Text } from "@chakra-ui/react";
+import { Box, Button, Flex, Text, Center } from "@chakra-ui/react";
 import DonationBox from "../Profile/Components/DonationBox";
 import { useState, useEffect } from "react";
 import { fetchData, postData } from "../../utils/Request";
+import RequestPayout from "./RequestPayout";
 // import { Helmet } from "react-helmet";
 
 const Donate = () => {
   const [donations, setDonations] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [reversed, setReversed] = useState();
+  const [isOpen, setIsOpen] = useState(false);
 
   useEffect(() => {
     const response = fetchData("/payment/get-donations", {
@@ -42,20 +44,25 @@ const Donate = () => {
         <title>Donations | Quickk Dashboard</title>
       </Helmet> */}
       <Box maxW={["100%", "70%"]} mx={"auto"} py={"2em"}>
-        <Text></Text>
         {isLoading ? (
           <Text>Loading...</Text>
         ) : donations.length > 0 ? (
-          <Flex flexWrap={"wrap"} justifyContent={"space-evenly"}>
-            {reversed?.map((donation) => (
-              <DonationBox
-                key={donation.id}
-                amount={donation.amount}
-                date={donation.donatedAt}
-                message={donation.donationMessage}
-              />
-            ))}
-          </Flex>
+          <Box position={"relative"}>
+            <RequestPayout isOpen={isOpen} />
+            <Center my={"2em"}>
+              <Button onClick={() => setIsOpen(true)}>Request Payout</Button>
+            </Center>
+            <Flex flexWrap={"wrap"} justifyContent={"space-evenly"}>
+              {reversed?.map((donation) => (
+                <DonationBox
+                  key={donation.id}
+                  amount={donation.amount}
+                  date={donation.donatedAt}
+                  message={donation.donationMessage}
+                />
+              ))}
+            </Flex>
+          </Box>
         ) : (
           <Text>No donations yet</Text>
         )}
