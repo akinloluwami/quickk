0238adfa9831b3aa07748ed150182066a2916053
Obiabo Immanuel <54102389+yhoungdev@users.noreply.github.com>
2022-07-31 18:06:52 +0100

    feat: added loader to homepage

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect , useState } from "react";
 import "./Home.scss";
 import { Link } from "react-router-dom";
 import ContainerLayout from "../../Layouts/ContainerLayout.jsx/ContainerLayout";
@@ -7,16 +7,23 @@ import Section from './Section';
 import LightBox from "./LightBox";
 import Footer from './Footer';
 import Divider from "./Divider";
+import Loading from "../../components/HomeKits/Loading";
+import { Box } from '@chakra-ui/react';
 
 function Home() {
+  const [isLoading, setLoading] = useState(true);
+
   useEffect(() => {
+
     document.title = "Quickk - Setup your blog in 2 minutes";
+    setTimeout (() => setLoading('none') , 1000)
   }, []);
+
   return (
     <>
-      {/* <Helmet>
-        <title>Quickk - Setup your blog in 2 minutes</title>
-      </Helmet> */}
+      <Box display={isLoading}>
+        <Loading/>
+      </Box>
       <Hero />
       <Section/>
       <LightBox/>
